974eb0d1f675cb1cf44d962dbb9ccc24a8216a86
Cesar Del Solar <delsolar@gmail.com>
2020-07-01 18:18:37 -0400

    add ratings to display

--- liwords-ui/src/store/end_of_game.ts
+++ liwords-ui/src/store/end_of_game.ts
@@ -5,11 +5,12 @@ import {
 
 export const endGameMessage = (gee: GameEndedEvent) => {
   const scores = gee.getScoresMap();
+  const ratings = gee.getNewRatingsMap();
   console.log('scores are', scores);
   // const ratings = gee.getNewRatingsMap();
   const reason = gee.getEndReason();
-  const winner = gee.getWinner();
-  const loser = gee.getLoser();
+  let winner = gee.getWinner();
+  let loser = gee.getLoser();
   const tie = gee.getTie();
   let summary = '';
   // const message = `Game is over. Scores: ${JSON.stringify(
@@ -17,8 +18,13 @@ export const endGameMessage = (gee: GameEndedEvent) => {
   // )}, new ratings: ${JSON.stringify(ratings)}`;
   let summaryReason = '';
   let summaryAddendum = '';
+  if (tie) {
+    [winner, loser] = scores.keys();
+  }
   const winscore = scores.get(winner);
   const losescore = scores.get(loser);
+  const winrating = ratings.get(winner);
+  const loserating = ratings.get(loser);
 
   switch (reason) {
     case GameEndReason.STANDARD:
@@ -43,6 +49,9 @@ export const endGameMessage = (gee: GameEndedEvent) => {
     summary = `${summary}${winner} wins${summaryReason}. ${summaryAddendum}`;
   } else {
     summary = `${summary}tie game! ${summaryAddendum}`;
+  }
+  if (winrating || loserating) {
+    summary += ` New ratings: ${winner}: ${winrating}, ${loser}: ${loserating}`;
   }
   return summary;
 };
