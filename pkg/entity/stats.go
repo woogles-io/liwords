f167819057b6061a2b442241d6d495703dd3961b
Josh <joshuacastellano7@gmail.com>
2020-07-05 12:18:33 -0400

    Handled stat denominators more elegantly (if I say so myself)

--- pkg/entity/stats.go
+++ pkg/entity/stats.go
@@ -1,11 +1,10 @@
 package entity
 
 import (
-	"fmt"
-	"unicode"
-	"strings"
 	"errors"
+	"fmt"
 	pb "github.com/domino14/macondo/gen/api/proto/macondo"
+	"unicode"
 )
 
 type ListItem struct {
@@ -15,37 +14,22 @@ type ListItem struct {
 	GameId      string
 }
 
-type StatItemDataType int
+type StatItemType int
 
 const (
-	SingleType StatItemDataType = iota
+	SingleType StatItemType = iota
 	ListType
 	DualType
 )
 
-type StatItemProcessingType int
-
-const (
-	TurnType StatItemProcessingType = iota
-	GameType
-)
-
-type NotableItem struct {
-	Name               string
-	Description        string
-	Total              int
-	Minimum            int
-	Maximum            int
-	GameList           []string
-	AddFunction        func(*NotableItem, *pb.GameEvent)
-}
-
 type StatItem struct {
 	Name               string
 	Description        string
+	Minimum            int
+	Maximum            int
 	Total              int
-	DataType           StatItemDataType
-	ProcessingType     StatItemProcessingType
+	Type               StatItemType
+	Denominator        *StatItem
 	IsProfileStat      bool
 	List               []*ListItem
 	HasMeaningfulTotal bool
@@ -58,42 +42,47 @@ type Stats struct {
 	GamesPlayed   int
 	PlayerOneData []*StatItem
 	PlayerTwoData []*StatItem
-	NotableData   []*NotableItem
+	NotableData   []*StatItem
 }
 
-func InstantiateNewStats() (*Stats) {
+func InstantiateNewStats() *Stats {
 	return &Stats{
 		PlayerOneId:   1,
 		PlayerTwoId:   2,
-		GamesPlayed:   1,
+		GamesPlayed:   0,
 		PlayerOneData: instantiatePlayerData(),
 		PlayerTwoData: instantiatePlayerData(),
 		NotableData:   instantiateNotableData()}
 }
 
-func (stats *Stats) AddGameToStats(history *pb.GameHistory, id string) (error) {
+func (stats *Stats) AddGameToStats(history *pb.GameHistory, id string) error {
+
+	// Stats not computed on an event basis:
+	//   Wins
+	//   Score
+	//   Firsts
+	//   Vertical Openings per First
+	//   Horizontal Openings per First
 
 	// Loop through all the turns in the game history
 	turns := history.GetTurns()
 	for _, turn := range turns {
 		for _, event := range turn.Events {
-			if history.Players[0].Nickname == event.Nickname || 
-			   (history.Players[1].Nickname == event.Nickname && history.SecondWentFirst){
-				addTurnToStatItems(stats.PlayerOneData, event, id)
+			fmt.Println(event)
+			if history.Players[0].Nickname == event.Nickname ||
+				(history.Players[1].Nickname == event.Nickname && history.SecondWentFirst) {
+				addEventToStatItems(stats.PlayerOneData, event, id)
 			} else {
-				addTurnToStatItems(stats.PlayerTwoData, event, id)
+				addEventToStatItems(stats.PlayerTwoData, event, id)
 			}
-			addTurnToNotableItems(stats.NotableData, event)
+			addEventToStatItems(stats.NotableData, event, id)
 		}
 	}
 	confirmNotableItems(stats.NotableData, id)
-	
-	stats.GamesPlayed++
-
 	return nil
 }
 
-func (stats *Stats) AddStatsToStats(otherStats *Stats) (error) {
+func (stats *Stats) AddStatsToStats(otherStats *Stats) error {
 
 	if stats.PlayerOneId != otherStats.PlayerOneId && stats.PlayerOneId != otherStats.PlayerTwoId {
 		return errors.New("Stats do not share an identical PlayerOneId")
@@ -107,20 +96,20 @@ func (stats *Stats) AddStatsToStats(otherStats *Stats) (error) {
 		otherPlayerTwoData = otherStats.PlayerOneData
 	}
 
-	err := combinePlayerData(stats.PlayerOneData, otherPlayerOneData)
-	
+	err := combineStatItemLists(stats.PlayerOneData, otherPlayerOneData)
+
 	if err != nil {
 		return err
 	}
 
-	err = combinePlayerData(stats.PlayerTwoData, otherPlayerTwoData)
-	
+	err = combineStatItemLists(stats.PlayerTwoData, otherPlayerTwoData)
+
 	if err != nil {
 		return err
 	}
 
-	err = combineNotableData(stats.NotableData, otherStats.NotableData)
-	
+	err = combineStatItemLists(stats.NotableData, otherStats.NotableData)
+
 	if err != nil {
 		return err
 	}
@@ -128,38 +117,26 @@ func (stats *Stats) AddStatsToStats(otherStats *Stats) (error) {
 	return nil
 }
 
-func (stats *Stats) ToString() (string) {
-	s := "Player One Data:\n" + playerDataToString(stats.PlayerOneData)
-	s += "\nPlayer Two Data:\n" + playerDataToString(stats.PlayerTwoData)
-	s += "\nNotable Data:\n" + notableDataToString(stats.NotableData)
+func (stats *Stats) ToString() string {
+	s := "Player One Data:\n" + statItemListToString(stats.PlayerOneData)
+	s += "\nPlayer Two Data:\n" + statItemListToString(stats.PlayerTwoData)
+	s += "\nNotable Data:\n" + statItemListToString(stats.NotableData)
 	return s
 }
 
-func playerDataToString(playerData []*StatItem) (string) {
+func statItemListToString(data []*StatItem) string {
 	s := ""
-	for _, statItem := range playerData {
+	for _, statItem := range data {
 		s += statItemToString(statItem)
 	}
 	return s
 }
 
-func notableDataToString(notableData []*NotableItem) (string) {
-	s := ""
-	for _, notableItem := range notableData {
-		s += notableItemToString(notableItem)
-	}
-	return s
-}
-
-func statItemToString(statItem *StatItem) (string) {
+func statItemToString(statItem *StatItem) string {
 	return fmt.Sprintf("  %s:\n    Total: %d\n    List:\n%s", statItem.Name, statItem.Total, listItemToString(statItem.List))
 }
 
-func notableItemToString(notableItem *NotableItem) (string) {
-	return fmt.Sprintf("     %s: %d, %s\n", notableItem.Name, notableItem.Total, strings.Join(notableItem.GameList[:], ","))
-}
-
-func listItemToString(listStat []*ListItem) (string) {
+func listItemToString(listStat []*ListItem) string {
 	s := ""
 	for _, wordItem := range listStat {
 		s += fmt.Sprintf("      %s, %d, %d, %s\n", wordItem.Word, wordItem.Score, wordItem.Probability, wordItem.GameId)
@@ -168,111 +145,103 @@ func listItemToString(listStat []*ListItem) (string) {
 }
 
 func instantiatePlayerData() []*StatItem {
-	return []*StatItem{
-
-&StatItem{Name: "Bingos",
+	gamesStat := &StatItem{Name: "Games",
+		Description:        "The number of games played",
+		Total:              0,
+		Type:               SingleType,
+		Denominator:        nil,
+		IsProfileStat:      true,
+		List:               []*ListItem{},
+		HasMeaningfulTotal: true,
+		AddFunction:        addGames}
+	bingosStat := &StatItem{Name: "Bingos",
 		Description:        "The list of bingos played",
 		Total:              0,
-		DataType:            DualType,
-		ProcessingType: TurnType,
+		Type:               DualType,
+		Denominator:        gamesStat,
 		IsProfileStat:      true,
 		List:               []*ListItem{},
 		HasMeaningfulTotal: true,
-		AddFunction:        addBingos},
-
-&StatItem{Name: "Triple Triples",
+		AddFunction:        addBingos}
+	tripleTriplesStat := &StatItem{Name: "Triple Triples",
 		Description:        "Number of triple triples played",
 		Total:              0,
-		DataType:           DualType,
-		ProcessingType: TurnType,
-		IsProfileStat:      true,
+		Type:               DualType,
+		Denominator:        gamesStat,
+		IsProfileStat:      false,
 		List:               []*ListItem{},
 		HasMeaningfulTotal: true,
-		AddFunction:        addTripleTriples},
-
-&StatItem{Name: "Bingo Nines or Above",
+		AddFunction:        addTripleTriples}
+	bingoNinesOrAboveStat := &StatItem{Name: "Bingo Nines or Above",
 		Description:        "The list of bingos that were nines tiles or above",
 		Total:              0,
-		DataType:           DualType,
-		ProcessingType: TurnType,
-		IsProfileStat:      true,
+		Type:               DualType,
+		Denominator:        gamesStat,
+		IsProfileStat:      false,
 		List:               []*ListItem{},
 		HasMeaningfulTotal: true,
-		AddFunction:        addBingoNinesOrAbove}}
+		AddFunction:        addBingoNinesOrAbove}
+
+	return []*StatItem{gamesStat,
+		bingosStat,
+		tripleTriplesStat,
+		bingoNinesOrAboveStat}
 }
 
-func instantiateNotableData() []*NotableItem {
-	return []*NotableItem{&NotableItem{Name: "No Blanks Played",
+func instantiateNotableData() []*StatItem {
+	return []*StatItem{&StatItem{Name: "No Blanks Played",
 		Description:        "Games in which no blanks are played",
-		Minimum: 0,
-		Maximum: 0,
+		Minimum:            0,
+		Maximum:            0,
 		Total:              0,
-		GameList:           []string{},
-		AddFunction:        addNoBlanksPlayed}}
+		Type:               ListType,
+		IsProfileStat:      false,
+		List:               []*ListItem{},
+		HasMeaningfulTotal: false,
+		AddFunction:        addBlanksPlayed}}
 }
 
-func addTurnToStatItems(statItems []*StatItem, event *pb.GameEvent, id string) {
+func addEventToStatItems(statItems []*StatItem, event *pb.GameEvent, id string) {
 	for _, statItem := range statItems {
 		statItem.AddFunction(statItem, event, id)
 	}
 }
 
-func addTurnToNotableItems(notableItems []*NotableItem, event *pb.GameEvent) {
-	for _, notableItem := range notableItems {
-		notableItem.AddFunction(notableItem, event)
-	}
-}
-
-func combinePlayerData(statItems []*StatItem, otherStatItems []*StatItem) error {
+func combineStatItemLists(statItems []*StatItem, otherStatItems []*StatItem) error {
 	if len(statItems) != len(otherStatItems) {
 		return errors.New("StatItem lists do not match in length")
 	}
 	for i := 0; i < len(statItems); i++ {
 		item := statItems[i]
 		itemOther := otherStatItems[i]
-		if item.Description != itemOther.Description {
-			return errors.New("StatItem descriptions do not match")
+		if item.Name != itemOther.Name {
+			return errors.New("StatItem names do not match")
 		}
 		combineItems(item, itemOther)
 	}
 	return nil
 }
 
-func combineNotableData(notableItems []*NotableItem, otherNotableItems []*NotableItem) error {
-	if len(notableItems) != len(otherNotableItems) {
-		return errors.New("NotableItem lists do not match in length")
-	}
-	for i := 0; i < len(notableItems); i++ {
-		item := notableItems[i]
-		itemOther := otherNotableItems[i]
-		if item.Description != itemOther.Description {
-			return errors.New("NotableItem descriptions do not match")
-		}
-		combineNotableItems(item, itemOther)
-	}
-	return nil
-}
-
-func confirmNotableItems(notableItems []*NotableItem, id string) {
-	for i := 0; i < len(notableItems); i++ {
-		item := notableItems[i]
+func confirmNotableItems(statItems []*StatItem, id string) {
+	for i := 0; i < len(statItems); i++ {
+		item := statItems[i]
 		if item.Total >= item.Minimum && item.Total <= item.Maximum {
-			item.GameList = append(item.GameList, id)
+			item.List = append(item.List, &ListItem{Word: "", Probability: 0, Score: 0, GameId: id})
 		}
 		item.Total = 0
 	}
 }
 
 func combineItems(statItem *StatItem, otherStatItem *StatItem) {
-	if statItem.DataType == SingleType {
+	if statItem.Type == SingleType {
 		statItem.Total += otherStatItem.Total
 	} else {
 		statItem.List = append(statItem.List, otherStatItem.List...)
 	}
 }
 
-func combineNotableItems(notableItem *NotableItem, otherNotableItem *NotableItem) {
-	notableItem.GameList = append(notableItem.GameList, otherNotableItem.GameList...)
+func addGames(statItem *StatItem, event *pb.GameEvent, id string) {
+	incrementStatItem(statItem, event, id)
 }
 
 func addBingos(statItem *StatItem, event *pb.GameEvent, id string) {
@@ -294,23 +263,21 @@ func addBingoNinesOrAbove(statItem *StatItem, event *pb.GameEvent, id string) {
 }
 
 func incrementStatItem(statItem *StatItem, event *pb.GameEvent, id string) {
-	if statItem.DataType == SingleType {
+	if statItem.Type == SingleType || statItem.Type == DualType {
 		statItem.Total++
-	} else if statItem.DataType == ListType {
-		statItem.List = append(statItem.List, &ListItem{Word:"YEET", Probability: 1, Score: 1, GameId: id})
-	} else if statItem.DataType == DualType {
-		statItem.Total++
-		statItem.List = append(statItem.List, &ListItem{Word:"YEET", Probability: 1, Score: 1, GameId: id})
+	}
+	if statItem.Type == ListType || statItem.Type == DualType {
+		statItem.List = append(statItem.List, &ListItem{Word: "YEET", Probability: 1, Score: 1, GameId: id})
 	}
 }
 
-func addNoBlanksPlayed(notableItem *NotableItem, event *pb.GameEvent) {
+func addBlanksPlayed(statItem *StatItem, event *pb.GameEvent, id string) {
 	tiles := event.PlayedTiles
-    for _, char := range tiles {
-    	if unicode.IsLower(char) {
-			notableItem.Total++
-    	}
-    }
+	for _, char := range tiles {
+		if unicode.IsLower(char) {
+			statItem.Total++
+		}
+	}
 }
 
 func isTripleTriple(event *pb.GameEvent) bool {
@@ -327,13 +294,13 @@ func isBingoNineOrAbove(event *pb.GameEvent) bool {
 	return isBingo(event) && len(event.PlayedTiles) >= 9
 }
 
-func getNumberOfTilesPlayed(play string) (int) {
+func getNumberOfTilesPlayed(play string) int {
 	sum := 0
-    for _, char := range play {
-    	// Unicode nonsense, Cesar plz help
-    	if !unicode.IsPunct(char) {
+	for _, char := range play {
+		// Unicode nonsense, Cesar plz help
+		if !unicode.IsPunct(char) {
 			sum++
-    	}
-    }
-    return sum
+		}
+	}
+	return sum
 }
