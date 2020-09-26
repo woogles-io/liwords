225e22bba33a08c0e888a4b42b4e4446a1feaf06
Cesar Del Solar <delsolar@gmail.com>
2020-09-26 01:46:05 -0400

    fix more data races

--- pkg/stores/soughtgame/memory.go
+++ pkg/stores/soughtgame/memory.go
@@ -18,7 +18,7 @@ var (
 // implementation, we will probably use a Postgres-backed store, or perhaps
 // something in Redis.
 type MemoryStore struct {
-	sync.Mutex
+	sync.RWMutex
 
 	soughtGames             map[string]*entity.SoughtGame
 	soughtGamesByUser       map[string]*entity.SoughtGame
@@ -35,6 +35,8 @@ func NewMemoryStore() *MemoryStore {
 
 // Get gets the game with the given ID.
 func (m *MemoryStore) Get(ctx context.Context, id string) (*entity.SoughtGame, error) {
+	m.RLock()
+	defer m.RUnlock()
 	g, ok := m.soughtGames[id]
 	if !ok {
 		return nil, errNotFound
@@ -98,13 +100,14 @@ func (m *MemoryStore) deleteFromReqsByReceiver(g *entity.SoughtGame) {
 
 // DeleteForUser deletes the game by seeker ID.
 func (m *MemoryStore) DeleteForUser(ctx context.Context, userID string) (string, error) {
+	m.Lock()
+	defer m.Unlock()
+
 	game, ok := m.soughtGamesByUser[userID]
 	if !ok {
 		// Do nothing, game never existed
 		return "", nil
 	}
-	m.Lock()
-	defer m.Unlock()
 	delete(m.soughtGamesByUser, userID)
 	delete(m.soughtGames, game.ID())
 	if game.Type() == entity.TypeMatch {
@@ -115,6 +118,8 @@ func (m *MemoryStore) DeleteForUser(ctx context.Context, userID string) (string,
 
 // ListOpenSeeks lists all open seek requests
 func (m *MemoryStore) ListOpenSeeks(ctx context.Context) ([]*entity.SoughtGame, error) {
+	m.RLock()
+	defer m.RUnlock()
 	ret := []*entity.SoughtGame{}
 	for _, v := range m.soughtGames {
 		if v.Type() == entity.TypeSeek {
@@ -125,6 +130,9 @@ func (m *MemoryStore) ListOpenSeeks(ctx context.Context) ([]*entity.SoughtGame, 
 }
 
 func (m *MemoryStore) ListOpenMatches(ctx context.Context, receiver string) ([]*entity.SoughtGame, error) {
+	m.RLock()
+	defer m.RUnlock()
+
 	ret := []*entity.SoughtGame{}
 	for _, v := range m.matchRequestsByReceiver[receiver] {
 		if v.Type() != entity.TypeMatch {
@@ -137,12 +145,16 @@ func (m *MemoryStore) ListOpenMatches(ctx context.Context, receiver string) ([]*
 
 // ExistsForUser returns true if the user already has an outstanding seek request.
 func (m *MemoryStore) ExistsForUser(ctx context.Context, userID string) (bool, error) {
+	m.RLock()
+	defer m.RUnlock()
 	_, ok := m.soughtGamesByUser[userID]
 	return ok, nil
 }
 
 // UserMatchedBy returns true if there is an open seek request from matcher for user
 func (m *MemoryStore) UserMatchedBy(ctx context.Context, userID, matcher string) (bool, error) {
+	m.RLock()
+	defer m.RUnlock()
 	matches, ok := m.matchRequestsByReceiver[userID]
 	if !ok {
 		return false, nil
