d34f446dc4ca6eca722c89514d71065eb8b6bbae
Andy Kurnia <andykurn@gmail.com>
2020-10-31 15:48:10 +0800

    show formatted suggestions

--- liwords-ui/src/gameroom/game_controls.tsx
+++ liwords-ui/src/gameroom/game_controls.tsx
@@ -19,6 +19,19 @@ import { fetchAndPrecache, getMacondo } from '../wasm/loader';
 
 const unrace = new Unrace();
 
+// See analyzer/analyzer.go JsonMove.
+type JsonMove = {
+  Action: string;
+  Row: number; // int
+  Column: number; // int
+  Vertical: boolean;
+  DisplayCoordinates: string;
+  Tiles: string;
+  Leave: string;
+  Equity: number; // float64
+  Score: number; // int
+};
+
 const filesByLexicon = [
   {
     lexicons: ['CSW19', 'NWL18'],
@@ -94,10 +107,100 @@ const ExamineGameControls = React.memo((props: { lexicon: string }) => {
 
       const boardStr = JSON.stringify(boardObj);
       const movesStr = await macondo.analyze(boardStr);
-      const movesObj = JSON.parse(movesStr);
+      const movesObj = JSON.parse(movesStr) as Array<JsonMove>;
 
       // Just log for now.
       console.log(movesObj); // for debugging
+
+      const suggestions = [];
+      for (const move of movesObj) {
+        if (move.Action === 'Play') {
+          // move.Tiles may have '.', read the board to complete it.
+          let tiles = '';
+          let r = move.Row;
+          let c = move.Column;
+          let inParen = false;
+          for (const t of move.Tiles) {
+            if (t === '.') {
+              if (!inParen) {
+                tiles += '(';
+                inParen = true;
+              }
+              tiles += letters[r * dim + c];
+            } else {
+              if (inParen) {
+                tiles += ')';
+                inParen = false;
+              }
+              tiles += t;
+            }
+            if (move.Vertical) ++r;
+            else ++c;
+          }
+          if (inParen) tiles += ')';
+          suggestions.push({
+            move: `${move.DisplayCoordinates} ${tiles}`,
+            score: move.Score,
+            leave: move.Leave,
+            valuation: move.Equity,
+          });
+        } else if (move.Action === 'Exchange' && move.Score === 0) {
+          suggestions.push({
+            move: `Exch. ${move.Tiles}`,
+            score: 0,
+            leave: move.Leave,
+            valuation: move.Equity,
+          });
+        } else if (move.Action === 'Pass' && move.Score === 0) {
+          suggestions.push({
+            move: 'Pass',
+            score: 0,
+            leave: move.Leave,
+            valuation: move.Equity,
+          });
+        } else if (move.Action === '' && move.Score === 0) {
+          // This happens at end of game. Ignore.
+        } else {
+          throw new Error(`unhandled case`);
+        }
+      }
+      let lastValuation = 0;
+      let lastRank = 1;
+      const suggestionStrs = suggestions.map(
+        ({ move, score, leave, valuation }, idx) => {
+          if (lastValuation !== valuation) {
+            lastValuation = valuation;
+            lastRank = idx + 1;
+          }
+          return [
+            String(lastRank),
+            move,
+            String(score),
+            leave,
+            valuation.toFixed(1),
+          ] as Array<string>;
+        }
+      );
+      const suggestionMaxLengths = Array.from(new Array(5), (_, idx) =>
+        suggestionStrs.reduce(
+          (maxLength, suggestionStr) =>
+            Math.max(maxLength, suggestionStr[idx].length),
+          0
+        )
+      );
+      const formattedSuggestions = suggestionStrs.map(
+        (suggestionStr) =>
+          `${suggestionStr[0].padStart(
+            suggestionMaxLengths[0]
+          )} ${suggestionStr[1].padEnd(
+            suggestionMaxLengths[1]
+          )} ${suggestionStr[2].padStart(
+            suggestionMaxLengths[2]
+          )} ${suggestionStr[3].padEnd(
+            suggestionMaxLengths[3]
+          )} ${suggestionStr[4].padStart(suggestionMaxLengths[4])}`
+      );
+      console.log(formattedSuggestions.join('\n'));
     })();
   }, [examinableGameContext, lexicon]);
 
