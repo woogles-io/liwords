5cf3bd1227fdbbf96e9f8a174d132c20ab30b775
Andy Kurnia <andykurn@gmail.com>
2020-12-16 12:11:06 +0800

    Revert "Merge pull request #310 from domino14/revert-309-fix-null-premove"
    This reverts commit 63cb6813442c76f4eaa679dab39ad6936f04d4c6, reversing
    changes made to 028144908e42a5c1a731b8b14a81c1a601f63602.

--- liwords-ui/src/gameroom/board_panel.tsx
+++ liwords-ui/src/gameroom/board_panel.tsx
@@ -360,47 +360,33 @@ export const BoardPanel = React.memo((props: Props) => {
   }, [setDisplayedRack, displayedRack]);
 
   const lastLettersRef = useRef<string>();
-  const readOnlyEffectDependenciesRef = useRef<{
-    displayedRack: string;
-    isMyTurn: () => boolean;
-    placedTiles: Set<EphemeralTile>;
-    dim: number;
-  }>();
-  readOnlyEffectDependenciesRef.current = {
-    displayedRack,
-    isMyTurn,
-    placedTiles,
-    dim: props.board.dim,
-  };
+  const lastMyTurnRef = useRef<boolean>();
 
   // Need to sync state to props here whenever the board changes.
   useEffect(() => {
     let fullReset = false;
     const lastLetters = lastLettersRef.current;
-    const dep = readOnlyEffectDependenciesRef.current!;
+    const wasMyTurn = lastMyTurnRef.current;
+    const nowMyTurn = isMyTurn();
     if (lastLetters === undefined) {
       // First load.
       fullReset = true;
-    } else if (
-      props.currentRack &&
-      !dep.displayedRack &&
-      !dep.placedTiles.size
-    ) {
+    } else if (props.currentRack && !displayedRack && !placedTiles.size) {
       // First load after receiving rack.
       fullReset = true;
     } else if (isExamining) {
       // Prevent stuck tiles.
       fullReset = true;
-    } else if (!dep.isMyTurn()) {
-      // Opponent's turn means we have just made a move. (Assumption: there are only two players.)
+    } else if (wasMyTurn && !nowMyTurn) {
+      // We have just made a move.
       fullReset = true;
     } else {
       // Opponent just did something. Check if it affects any premove.
       // TODO: revisit when supporting non-square boards.
       const letterAt = (row: number, col: number, letters: string) =>
-        row < 0 || row >= dep.dim || col < 0 || col >= dep.dim
+        row < 0 || row >= props.board.dim || col < 0 || col >= props.board.dim
           ? null
-          : letters[row * dep.dim + col];
+          : letters[row * props.board.dim + col];
       const letterChanged = (row: number, col: number) =>
         letterAt(row, col, lastLetters) !==
         letterAt(row, col, props.board.letters);
@@ -426,10 +412,14 @@ export const BoardPanel = React.memo((props: Props) => {
         hookChanged(row, col, +1, 0) ||
         hookChanged(row, col, 0, -1) ||
         hookChanged(row, col, 0, +1);
+      // If no tiles have been placed, but placement arrow is shown,
+      // reset based on if that position is affected.
+      // This avoids having the placement arrow behind a tile.
       if (
-        Array.from(dep.placedTiles).some(({ row, col }) =>
-          placedTileAffected(row, col)
-        )
+        (placedTiles.size === 0 && arrowProperties.show
+          ? [arrowProperties as { row: number; col: number }]
+          : Array.from(placedTiles)
+        ).some(({ row, col }) => placedTileAffected(row, col))
       ) {
         fullReset = true;
       }
@@ -441,8 +431,14 @@ export const BoardPanel = React.memo((props: Props) => {
       setArrowProperties({ row: 0, col: 0, horizontal: false, show: false });
     }
     lastLettersRef.current = props.board.letters;
+    lastMyTurnRef.current = nowMyTurn;
   }, [
+    arrowProperties,
+    displayedRack,
     isExamining,
+    isMyTurn,
+    placedTiles,
+    props.board.dim,
     props.board.letters,
     props.currentRack,
     setDisplayedRack,
