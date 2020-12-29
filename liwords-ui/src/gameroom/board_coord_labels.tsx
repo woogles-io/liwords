f8bb24d25f507d8c3791d1986eed6df8d7162ea0
Cesar Del Solar <delsolar@gmail.com>
2020-12-28 22:46:17 -0500

    run prettier

--- liwords-ui/src/gameroom/board_coord_labels.tsx
+++ liwords-ui/src/gameroom/board_coord_labels.tsx
@@ -12,7 +12,7 @@ const BoardCoordLabels = React.memo((props: Props) => {
 
   // COLUMN labels.
   for (let i = 0; i < props.gridDim; i += 1) {
-      horizLabels.push(
+    horizLabels.push(
       <CoordLabel
         label={String.fromCharCode(i + 'A'.charCodeAt(0))}
         key={`collbl${i}`}
@@ -22,21 +22,14 @@ const BoardCoordLabels = React.memo((props: Props) => {
 
   // ROW labels
   for (let i = 0; i < props.gridDim; i += 1) {
-      vertLabels.push(
-      <CoordLabel
-        label={String(i + 1)}
-        key={`rowlbl${i}`}
-      />
-    );
+    vertLabels.push(<CoordLabel label={String(i + 1)} key={`rowlbl${i}`} />);
   }
-  return <>
-    <div className="coord-labels horiz">
-      {horizLabels}
-    </div>
-    <div className="coord-labels vert">
-      {vertLabels}
-    </div>
-  </>;
+  return (
+    <>
+      <div className="coord-labels horiz">{horizLabels}</div>
+      <div className="coord-labels vert">{vertLabels}</div>
+    </>
+  );
 });
 
 export default BoardCoordLabels;
