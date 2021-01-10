b277ea5bda1895b25face9ff71b55e0ce9f39f28
Seth Lipkin <seth.lipkin@gmail.com>
2021-01-10 15:59:58 -0500

    Added a markdown example

--- liwords-ui/src/profile/bio.tsx
+++ liwords-ui/src/profile/bio.tsx
@@ -3,8 +3,9 @@ import ReactMarkdown from 'react-markdown';
 import { useMountedState } from '../utils/mounted';
 import { useParams } from 'react-router-dom';
 import { useLoginStateStoreContext } from '../store/store';
-import { Card, Modal, Row, Col, Form, Input, Alert, Button } from 'antd';
+import { Card, Modal, Table, Row, Col, Form, Input, Alert, Button } from 'antd';
 import { TopBar } from '../topbar/topbar';
+import './bio.scss';
 
 type BioProps = {
   bio: string;
@@ -12,7 +13,7 @@ type BioProps = {
 
 export const BioCard = React.memo((props: BioProps) => {
   const { useState } = useMountedState();
-  const [bioEditVisible, setBioEditVisible] = useState(false);
+  const [bioEditVisible, setBioEditVisible] = useState(true);
   const { loginState } = useLoginStateStoreContext();
   const { username: viewer } = loginState;
   const { username } = useParams();
@@ -30,6 +31,32 @@ export const BioCard = React.memo((props: BioProps) => {
       )] 
     : []
   
+  const dataSource = [
+    {
+      key: '1',
+      type: 'Italics',
+      use: 'single asterisks',
+      example: '*hello*',
+      result: <ReactMarkdown>*hello*</ReactMarkdown>
+    },
+    {
+      key: '2',
+      type: 'Bold',
+      use: 'double asterisks',
+      example: '**hello**',
+      result: <ReactMarkdown>**hello**</ReactMarkdown>
+    },
+  ];
+
+  const columns = [
+    { title: 'To get', dataIndex: 'type' },
+    { title: 'Use', dataIndex: 'use' },
+    { title: 'Example', dataIndex: 'example' },
+    { title: 'Result', dataIndex: 'result' }
+  ];
+
+  const foobar="hello!";
+
   return (
     <Card title="Bio" actions={actions}>
       <ReactMarkdown>{props.bio}</ReactMarkdown>
@@ -39,6 +66,7 @@ export const BioCard = React.memo((props: BioProps) => {
         onCancel={() => {
           setBioEditVisible(false);
         }}
+        className="bio-edit-modal"
       >
         <Form>
           <Form.Item
@@ -46,10 +74,24 @@ export const BioCard = React.memo((props: BioProps) => {
             name="bioMarkup"
           >
             <Input
+              value='Blah'
               placeholder="Enter bio in Markdown form"
             />
           </Form.Item>
         </Form>
+
+      <div className="preview">
+        <div>How your bio will look to others:</div>
+        <Card className="preview-card">
+        <ReactMarkdown>{props.bio}</ReactMarkdown>
+        </Card>
+      </div>
+      <Table 
+        title={() => 'Markdown Tips'}
+        dataSource={dataSource} 
+        columns={columns} 
+        pagination={{hideOnSinglePage: true}}
+      />
       </Modal>
     </Card>
   );
