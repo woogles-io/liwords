73b683dbf37042d6168ff619291fd4b71db97de6
Seth Lipkin <seth.lipkin@gmail.com>
2021-02-01 23:26:01 -0500

    Works end to end if very small(!) images are used.

--- pkg/user/uploader.go
+++ pkg/user/uploader.go
@@ -1,12 +1,16 @@
 package user
 
 import (
-	// "bytes"
+    "bytes"
+	b64 "encoding/base64"
 	"context"
-	"errors"
-	"os"
+    "encoding/json"
+    "errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 
-	"github.com/lithammer/shortuuid"
+	"github.com/rs/zerolog/log"
 )
 
 type UploadService interface {
@@ -20,24 +24,48 @@ func NewXTUploadService() (*XTUploadService) {
 	return &XTUploadService{}
 }
 
+type UploadResult struct {
+    AvatarUrl    string   `json:"avatar_url,omitempty"`
+}
+
 // Upload takes in JPG bytes
 func (s *XTUploadService) Upload(ctx context.Context, prefix string, data []byte) (string, error) {
-	id := prefix + "-" + shortuuid.New()[1:10] + ".jpg"
+	// Base-64 encode the image data and then URLEncode that.
+	sEnc := url.QueryEscape(b64.StdEncoding.EncodeToString([]byte(data)))
 
-	// Store the file with a name reflective of the user's UUID
-	// filename := user.UUID + ".jpg"
-	// avatarUrl := "file:///Users/slipkin/Projects/woogles/liwords/" + filename
+	// Send the photo data as a parameter in the URL itself. This is terrible.
+	url := "http://cross-tables.com/rest/uploadavatar.php?prefix=" + prefix + "&photobytes=" + sEnc
 
-	f, createErr := os.Create(id)
-	if createErr != nil {
-		return "", errors.New("Cannot create local file")
+	resp, err := http.Get(url)
+	if err != nil {
+        return "", err
 	}
 
-	_, writeErr := f.WriteString(string(data))
-	if writeErr != nil {
-		return "", errors.New("Cannot write local file")
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+        return "", errors.New("Unable to update avatar")
 	}
 
-	avatarUrl := "file:///Users/slipkin/Projects/woogles/liwords/" + id
-	return avatarUrl, nil
+    bodyBytes, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return "", err
+    }
+
+    r := bytes.NewReader(bodyBytes)
+    decoder := json.NewDecoder(r)
+
+    val := &UploadResult{}
+    decodeErr := decoder.Decode(val)
+
+	if decodeErr != nil {
+		log.Error().Err(decodeErr)
+	    return "", decodeErr
+    }
+
+	if len(val.AvatarUrl) == 0 {
+        return "", errors.New("Unable to update avatar")
+	}
+
+	return val.AvatarUrl, nil
 }
