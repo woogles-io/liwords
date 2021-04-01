9ecb3a4f5ad06b2965118985a3d359ab43605f4c
César Del Solar <delsolar@gmail.com>
2021-03-31 23:18:11 -0400

    Forgot this file

--- liwords-ui/public/config.js
+++ liwords-ui/public/config.js
@@ -5,4 +5,8 @@ window.RUNTIME_CONFIGURATION = {
   macondoFilename: 'macondo.wasm',
   stripePublishableKey:
     'pk_test_51I7T0HH0ARGCjmpLQHlwHvRjcOdNj2jra4B8tkttJB8bCUwmdWQt2jg0GFB2lJmyUGexhYjwp7fnLKiizo0LcSMG00sLClcmT8',
+  stripePrices: {
+    5: 'price_1Iaq8HH0ARGCjmpLEIMQhzEj',
+    50: 'price_1Iaq8eH0ARGCjmpLeoH9CCGi',
+  },
 };
