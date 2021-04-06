12b85f3a1489c87bed801e9b1ed3072f44cea33b
Cesar Del Solar <delsolar@gmail.com>
2021-04-05 23:49:22 -0400

    formatting

--- liwords-ui/src/utils/cwgame/blindfold.ts
+++ liwords-ui/src/utils/cwgame/blindfold.ts
@@ -1,14 +1,14 @@
 /** @fileoverview business logic for handling blindfold events */
 
 export type BlindfoldCoordinates = {
-  row: number,
-  col: number,
-  horizontal: boolean
+  row: number;
+  col: number;
+  horizontal: boolean;
 };
 
 export const parseBlindfoldCoordinates = (
-  coordinates: string) : BlindfoldCoordinates | undefined => {
-
+  coordinates: string
+): BlindfoldCoordinates | undefined => {
   const horizontalRegex = /^([0-9][0-9]?)([A-O])$/;
   const matches = coordinates.match(horizontalRegex);
   let row = -1;
@@ -16,48 +16,48 @@ export const parseBlindfoldCoordinates = (
   let horizontal = false;
   if (matches && matches[1] !== undefined && matches[2] !== undefined) {
     row = parseInt(matches[1]) - 1;
-    col = matches[2].charCodeAt(0)-65;
+    col = matches[2].charCodeAt(0) - 65;
     horizontal = true;
   } else {
     const verticalRegex = /^([A-O])([0-9][0-9]?)$/;
     const matches = coordinates.match(verticalRegex);
     if (matches && matches[1] !== undefined && matches[2] !== undefined) {
       row = parseInt(matches[2]) - 1;
-      col = matches[1].charCodeAt(0)-65;
+      col = matches[1].charCodeAt(0) - 65;
       horizontal = false;
     }
   }
   if (row < 0) {
-    return undefined
+    return undefined;
   }
-  return {row: row, col: col, horizontal: horizontal};
-}
+  return { row: row, col: col, horizontal: horizontal };
+};
 
 export const natoPhoneticAlphabet = new Map([
-    ['A', 'Alpha' ],
-    ['B', 'Bravo'],
-    ['C', 'Charlie'],
-    ['D', 'Delta'],
-    ['E', 'Echo'],
-    ['F', 'Foxtrot'],
-    ['G', 'Golf'],
-    ['H', 'Hotel'],
-    ['I', 'India'],
-    ['J', 'Juliett'],
-    ['K', 'Kilo'],
-    ['L', 'Lima'],
-    ['M', 'Mike'],
-    ['N', 'November'],
-    ['O', 'Oscar'],
-    ['P', 'Papa'],
-    ['Q', 'Quebec'],
-    ['R', 'Romeo'],
-    ['S', 'Sierra'],
-    ['T', 'Tango'],
-    ['U', 'Uniform'],
-    ['V', 'Victor'],
-    ['W', 'Whiskey'],
-    ['X', 'X-ray'],
-    ['Y', 'Yankee'],
-    ['Z', 'Zulu']
-  ]);
+  ['A', 'Alpha'],
+  ['B', 'Bravo'],
+  ['C', 'Charlie'],
+  ['D', 'Delta'],
+  ['E', 'Echo'],
+  ['F', 'Foxtrot'],
+  ['G', 'Golf'],
+  ['H', 'Hotel'],
+  ['I', 'India'],
+  ['J', 'Juliett'],
+  ['K', 'Kilo'],
+  ['L', 'Lima'],
+  ['M', 'Mike'],
+  ['N', 'November'],
+  ['O', 'Oscar'],
+  ['P', 'Papa'],
+  ['Q', 'Quebec'],
+  ['R', 'Romeo'],
+  ['S', 'Sierra'],
+  ['T', 'Tango'],
+  ['U', 'Uniform'],
+  ['V', 'Victor'],
+  ['W', 'Whiskey'],
+  ['X', 'X-ray'],
+  ['Y', 'Yankee'],
+  ['Z', 'Zulu'],
+]);
