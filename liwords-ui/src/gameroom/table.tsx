0c4528cd94ed3ba471c70d033f34a77960a58142
Cesar Del Solar <delsolar@gmail.com>
2021-05-14 01:23:13 -0400

    remove OSPD6

--- liwords-ui/src/gameroom/table.tsx
+++ liwords-ui/src/gameroom/table.tsx
@@ -567,8 +567,6 @@ export const Table = React.memo((props: Props) => {
             ? ['CSW19', 'NWL20']
             : lexicon === 'CSW19X'
             ? ['CSW19']
-            : lexicon === 'OSPD6'
-            ? ['NWL20', 'CSW19']
             : []) {
             const wordsToRedefine = [];
             for (const word of wordsToDefine) {
