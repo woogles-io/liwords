f77323f1141298229f83d9deb63743ffc67a5ca5
Andy Kurnia <andykurn@gmail.com>
2021-06-03 14:16:35 +0800

    update wolges-wasm to 5c1538fec273cdc51f1c3c2d23c1393090285526

--- liwords-ui/wolges-wasm-pkg/wolges_wasm_bg.wasm.d.ts
+++ liwords-ui/wolges-wasm-pkg/wolges_wasm_bg.wasm.d.ts
@@ -5,6 +5,7 @@ export function do_this_on_startup(): void;
 export function precache_kwg(a: number, b: number, c: number, d: number): void;
 export function precache_klv(a: number, b: number, c: number, d: number): void;
 export function analyze(a: number, b: number): number;
+export function play_score(a: number, b: number): number;
 export const __wbindgen_export_0: WebAssembly.Table;
 export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1716e9bca73978f3(a: number, b: number, c: number): void;
 export function __wbindgen_malloc(a: number): number;
