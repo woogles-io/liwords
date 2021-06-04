babb611a9dcf259f12026727cc43ab11db086c2c
Andy Kurnia <andykurn@gmail.com>
2021-06-05 01:55:26 +0800

    optimize simple timer

--- liwords-ui/src/gameroom/simple_timer.tsx
+++ liwords-ui/src/gameroom/simple_timer.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useMountedState } from '../utils/mounted';
 
 // This magical timer was written by Andy. I am not sure how it works.
@@ -12,24 +12,27 @@ export const SimpleTimer = ({
   isRunning: boolean;
 }) => {
   const { useState } = useMountedState();
-  const [rerender, setRerender] = useState([]);
-  void rerender;
-  const lastRaf = useRef(0);
-
-  const cb = useCallback(() => {
-    // this seems to force a rerender
-    setRerender([]);
-    lastRaf.current = requestAnimationFrame(cb);
-  }, []);
-
-  useEffect(() => {
-    cb();
-    return () => cancelAnimationFrame(lastRaf.current);
-  }, [cb]);
+  const [, setRerender] = useState(0);
+  const requestRerender = useCallback(
+    () => setRerender((n) => (n + 1) | 0),
+    []
+  );
 
   const currentMillis = isRunning
     ? millisAtLastRefresh - (performance.now() - lastRefreshedPerformanceNow)
     : millisAtLastRefresh;
+
+  useEffect(() => {
+    if (isRunning && currentMillis > 0) {
+      // compute when the display would change.
+      const validForMillis = ((currentMillis - 1) % 1000) + 1;
+      const t = setTimeout(() => {
+        requestRerender();
+      }, validForMillis);
+      return () => clearTimeout(t);
+    }
+  }); // no dependency list, this effect should run on every render.
+
   const currentSec = Math.ceil(currentMillis / 1000);
   const nonnegativeSec = Math.max(currentSec, 0);
   return <>{`${nonnegativeSec} second${nonnegativeSec === 1 ? '' : 's'}`}</>;
