ed3aa2ef40ee50d74388ccd8d34cb8e85ee5eadf
Andy Kurnia <andykurn@gmail.com>
2021-06-05 09:59:30 +0800

    remove redundant dragging state

--- liwords-ui/src/gameroom/tile.tsx
+++ liwords-ui/src/gameroom/tile.tsx
@@ -141,13 +141,8 @@ type TileProps = {
 };
 
 const Tile = React.memo((props: TileProps) => {
-  const { useState } = useMountedState();
-
-  const [isMouseDragging, setIsMouseDragging] = useState(false);
-
   const handleStartDrag = (e: any) => {
     if (e) {
-      setIsMouseDragging(true);
       e.dataTransfer.dropEffect = 'move';
       if (
         props.tentative &&
@@ -159,10 +154,6 @@ const Tile = React.memo((props: TileProps) => {
         e.dataTransfer.setData('rackIndex', props.rackIndex);
       }
     }
-  };
-
-  const handleEndDrag = () => {
-    setIsMouseDragging(false);
   };
 
   const handleDrop = (e: any) => {
@@ -253,13 +244,13 @@ const Tile = React.memo((props: TileProps) => {
     }
   }, [canDrop, isTouchDeviceResult, drop]);
 
-  const computedClassName = `tile${
-    isDragging || isMouseDragging ? ' dragging' : ''
-  }${canDrag ? ' droppable' : ''}${props.selected ? ' selected' : ''}${
-    props.tentative ? ' tentative' : ''
-  }${props.lastPlayed ? ' last-played' : ''}${
-    isDesignatedBlank(props.rune) ? ' blank' : ''
-  }${props.playerOfTile ? ' tile-p1' : ' tile-p0'}`;
+  const computedClassName = `tile${isDragging ? ' dragging' : ''}${
+    canDrag ? ' droppable' : ''
+  }${props.selected ? ' selected' : ''}${props.tentative ? ' tentative' : ''}${
+    props.lastPlayed ? ' last-played' : ''
+  }${isDesignatedBlank(props.rune) ? ' blank' : ''}${
+    props.playerOfTile ? ' tile-p1' : ' tile-p0'
+  }`;
   let ret = (
     <div onDragOver={handleDropOver} onDrop={handleDrop} ref={tileRef}>
       <div
@@ -273,7 +264,6 @@ const Tile = React.memo((props: TileProps) => {
         onMouseEnter={props.onMouseEnter}
         onMouseLeave={props.onMouseLeave}
         onDragStart={canDrag ? handleStartDrag : undefined}
-        onDragEnd={handleEndDrag}
         draggable={canDrag}
       >
         {props.rune !== EmptySpace && (
