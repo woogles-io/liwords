9f42c846489a25c0a1872081077da7c3ba7a2680
Cesar Del Solar <delsolar@gmail.com>
2021-07-20 18:23:15 -0400

    forgot to add ready button

---
+++ liwords-ui/src/tournament/ready_button.tsx
@@ -0,0 +1,22 @@
+import { Button } from 'antd';
+import React, { useState } from 'react';
+
+type Props = {
+  sendReady: () => void;
+};
+
+export const ReadyButton = (props: Props) => {
+  const [disabled, setDisabled] = useState(false);
+  return (
+    <Button
+      className="primary"
+      onClick={() => {
+        props.sendReady();
+        setDisabled(true);
+      }}
+      disabled={disabled}
+    >
+      I'm ready
+    </Button>
+  );
+};
