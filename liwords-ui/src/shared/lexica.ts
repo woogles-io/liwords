4f29816b35eac15a844c72407a5b937c75784dc5
Andy Kurnia <andykurn@gmail.com>
2022-01-20 22:09:26 +0800

    fix shown rating names

--- liwords-ui/src/shared/lexica.ts
+++ liwords-ui/src/shared/lexica.ts
@@ -2,10 +2,40 @@
  * @fileoverview A mapping between lexicon codes and user-visible lexicon names.
  */
 
+// internal rating name is saved in db (select ratings->'Data' from profiles).
+// it follows pkg/entity/ratings.go ToVariantKey.
+// XXX: foreign ratings aren't grouped by family.
+const lexiconCodeToInternalRatingName = (code: string) => {
+  if (code.startsWith('NWL')) return 'NWL18';
+  if (code.startsWith('CSW')) return 'CSW19';
+  if (code.startsWith('ECWL')) return 'ECWL';
+  return code;
+};
+
+// profile rating name is the name that shows up in your profile ratings.
+const InternalRatingNameToProfileRatingName: {
+  [code: string]: string;
+} = {
+  NWL18: 'NWL',
+  NSWL20: 'NSWL',
+  CSW19: 'CSW',
+  ECWL: 'CEL',
+  RD28: 'Deutsch',
+  NSF21: 'Norsk',
+  FRA20: 'Français',
+};
+
+export const lexiconCodeToProfileRatingName = (code: string) => {
+  const internalRatingName = lexiconCodeToInternalRatingName(code);
+  return (
+    InternalRatingNameToProfileRatingName[internalRatingName] ??
+    internalRatingName
+  );
+};
+
 type Lexicon = {
   code: string;
   shortDescription: string;
-  ratingName: string; // the name that shows up in your profile ratings
   matchName: string; // the name that shows up in a match/seek/watch
   longDescription?: string;
   flagCode?: string;
@@ -15,7 +45,6 @@ export const AllLexica: { [code: string]: Lexicon } = {
   NWL20: {
     code: 'NWL20',
     shortDescription: 'NWL 20 (North American English)',
-    ratingName: 'NWL',
     matchName: 'NWL20',
     longDescription:
       'NASPA Word List, 2020 Edition (NWL20), © 2020 North American Word Game Players Association. All rights reserved.',
@@ -25,7 +54,6 @@ export const AllLexica: { [code: string]: Lexicon } = {
   NSWL20: {
     code: 'NSWL20',
     shortDescription: 'NSWL 20 (NASPA School Word List)',
-    ratingName: 'NSWL',
     matchName: 'NSWL20',
     longDescription:
       'NASPA School Word List 2020 Edition (NSWL20), © 2020 North American Word Game Players Association. All rights reserved.',
@@ -33,7 +61,6 @@ export const AllLexica: { [code: string]: Lexicon } = {
   CSW21: {
     code: 'CSW21',
     shortDescription: 'CSW 21 (World English)',
-    ratingName: 'CSW',
     matchName: 'CSW21',
     longDescription:
       'Published under license with Collins, an imprint of HarperCollins Publishers Limited',
@@ -41,7 +68,6 @@ export const AllLexica: { [code: string]: Lexicon } = {
   ECWL: {
     code: 'ECWL',
     shortDescription: 'CEL (Common English Lexicon)',
-    ratingName: 'CEL',
     matchName: 'CEL',
     longDescription:
       'Common English Lexicon, Copyright (c) 2021 Fj00. Used with permission',
@@ -49,7 +75,6 @@ export const AllLexica: { [code: string]: Lexicon } = {
   RD28: {
     code: 'RD28',
     shortDescription: 'Deutsch (German)',
-    ratingName: 'Deutsch',
     matchName: 'Deutsch',
     longDescription:
       'The “Scrabble®-Turnierliste” used as the German Lexicon is subject to copyright and related rights of Scrabble® Deutschland e.V. With the friendly assistance of Gero Illings SuperDic.',
@@ -58,7 +83,6 @@ export const AllLexica: { [code: string]: Lexicon } = {
   NSF21: {
     code: 'NSF21',
     shortDescription: 'Norsk (Norwegian)',
-    ratingName: 'Norsk',
     matchName: 'Norsk',
     longDescription:
       'The NSF word list is provided by the language committee of the Norwegian Scrabble Player Association. Used with permission.',
@@ -67,14 +91,12 @@ export const AllLexica: { [code: string]: Lexicon } = {
   FRA20: {
     code: 'FRA20',
     shortDescription: 'Français (French)',
-    ratingName: 'Français',
     matchName: 'Français',
     flagCode: 'fr',
   },
   CSW19X: {
     code: 'CSW19X',
     shortDescription: 'CSW19X (School Expurgated)',
-    ratingName: 'CSW',
     matchName: 'CSW19X',
   },
 };
