7f6ac9b3f2c38b06985d1d5deef300278ecb373c
Cesar Del Solar <delsolar@gmail.com>
2022-03-23 22:48:13 -0400

    fix FE test

--- liwords-ui/src/store/reducers/turns.ts
+++ liwords-ui/src/store/reducers/turns.ts
@@ -8,13 +8,14 @@ export const gameEventsToTurns = (evts: Array<GameEvent>) => {
   let lastTurn: Turn = new Array<GameEvent>();
   evts.forEach((evt) => {
     // XXX: remove when we get rid of nicknames fully
-    let playersDiffer;
-    if (lastTurn[0].getNickname() !== '') {
-      playersDiffer = lastTurn[0].getNickname() !== evt.getNickname();
-    } else {
-      playersDiffer = lastTurn[0].getPlayerIndex() !== evt.getPlayerIndex();
+    let playersDiffer = false;
+    if (lastTurn.length !== 0) {
+      if (lastTurn[0].getNickname() !== '') {
+        playersDiffer = lastTurn[0].getNickname() !== evt.getNickname();
+      } else {
+        playersDiffer = lastTurn[0].getPlayerIndex() !== evt.getPlayerIndex();
+      }
     }
-
     if (
       (lastTurn.length !== 0 && playersDiffer) ||
       evt.getType() === GameEvent.Type.TIME_PENALTY ||
