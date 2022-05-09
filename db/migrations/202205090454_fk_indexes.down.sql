3ac10e5c7e2a444804bf6b326495491b936f21f2
Cesar Del Solar <delsolar@gmail.com>
2022-05-09 08:54:16 -0400

    add a couple more forgotten indexes

--- db/migrations/202205090454_fk_indexes.down.sql
+++ db/migrations/202205090454_fk_indexes.down.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
 DROP INDEX IF EXISTS profiles_user_id_idx;
+DROP INDEX IF EXISTS idx_followings_follower;
+DROP INDEX IF EXISTS idx_blockings_blocker;
 
 COMMIT;
--- db/migrations/202205090454_fk_indexes.up.sql
+++ db/migrations/202205090454_fk_indexes.up.sql
@@ -4,4 +4,14 @@ BEGIN;
 CREATE UNIQUE INDEX IF NOT EXISTS
     profiles_user_id_idx ON profiles(user_id);
 
+-- we already have a unique index on the combination of both columns,
+-- but we should add one on the second column for each of these
+-- (the first column index is implied by the unique index)
+
+CREATE INDEX IF NOT EXISTS idx_followings_follower ON public.followings
+    USING btree(follower_id);
+
+CREATE INDEX IF NOT EXISTS idx_blockings_blocker ON public.blockings
+    USING btree(blocker_id);
+
 COMMIT;
