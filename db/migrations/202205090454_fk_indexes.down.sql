d2263fbf49cd7f9f506eb3b63cf71ee0468faae9
Cesar Del Solar <delsolar@gmail.com>
2022-05-09 09:11:01 -0400

    more missing indexes

--- db/migrations/202205090454_fk_indexes.down.sql
+++ db/migrations/202205090454_fk_indexes.down.sql
@@ -4,4 +4,11 @@ DROP INDEX IF EXISTS profiles_user_id_idx;
 DROP INDEX IF EXISTS idx_followings_follower;
 DROP INDEX IF EXISTS idx_blockings_blocker;
 
+DROP INDEX IF EXISTS idx_puzzle_attempts_user;
+DROP INDEX IF EXISTS idx_puzzle_games;
+DROP INDEX IF EXISTS idx_puzzle_generationids;
+DROP INDEX IF EXISTS idx_puzzle_authorid;
+DROP INDEX IF EXISTS idx_puzzletags_tagid;
+DROP INDEX IF EXISTS idx_puzzlevotes_user_id;
+
 COMMIT;
--- db/migrations/202205090454_fk_indexes.up.sql
+++ db/migrations/202205090454_fk_indexes.up.sql
@@ -14,4 +14,25 @@ CREATE INDEX IF NOT EXISTS idx_followings_follower ON public.followings
 CREATE INDEX IF NOT EXISTS idx_blockings_blocker ON public.blockings
     USING btree(blocker_id);
 
+-- Other missing indexes
+-- See https://www.cybertec-postgresql.com/en/index-your-foreign-key/
+
+CREATE INDEX IF NOT EXISTS idx_puzzle_attempts_user ON public.puzzle_attempts
+    USING btree(user_id);
+
+CREATE INDEX IF NOT EXISTS idx_puzzle_games ON public.puzzles
+    USING btree(game_id);
+
+CREATE INDEX IF NOT EXISTS idx_puzzle_generationids ON public.puzzles
+    USING btree(generation_id);
+
+CREATE INDEX IF NOT EXISTS idx_puzzle_authorid ON public.puzzles
+    USING btree(author_id);
+
+CREATE INDEX IF NOT EXISTS idx_puzzletags_tagid ON public.puzzle_tags
+    USING btree(tag_id);
+
+CREATE INDEX IF NOT EXISTS idx_puzzlevotes_user_id ON public.puzzle_votes
+    USING btree(user_id);
+
 COMMIT;
