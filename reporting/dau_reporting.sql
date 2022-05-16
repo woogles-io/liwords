1a3a1dbe7861d1c312309e836d0a6f692bcdc985
Jesse Day <jessed@berkeley.edu>
2022-05-15 17:30:36 -0700

    DAU count now defined as omgwords+puzzles

--- reporting/dau_reporting.sql
+++ reporting/dau_reporting.sql
@@ -8,10 +8,10 @@ UNION ALL
     created_at,
     player1_id AS player
 FROM public.games)),
-dau_report AS
+dau_omgwords_report AS
 (SELECT
 	DATE_TRUNC('day',duplicated_games.created_at) AS day,
-	COUNT(DISTINCT player) AS dau
+	COUNT(DISTINCT player) AS dau_omgwords
 FROM duplicated_games
 LEFT JOIN public.users ON duplicated_games.player = users.id
 WHERE NOT (users.internal_bot OR users.id IN (42,43,44,45,46))
@@ -42,10 +42,10 @@ UNION ALL
     created_at,
     player1_id AS player
 FROM pvp_games)),
-dauh_report AS
+dau_omgwords_vs_human_report AS
 (SELECT
     DATE_TRUNC('day',created_at) AS day,
-	COUNT(DISTINCT player) AS dau_h
+	COUNT(DISTINCT player) AS dau_omgwords_vs_human
 FROM duplicated_games_between_humans
 GROUP BY 1),
 dau_puzzles AS
@@ -76,24 +76,24 @@ UNION ALL
    created_at,
    user_id AS player
 FROM public.puzzle_attempts)),
-games_plus_puzzles_report AS
+omgwords_plus_puzzles_report AS
 (SELECT
 	DATE_TRUNC('day',games_plus_puzzle_attempts.created_at) AS day,
-	COUNT(DISTINCT player) AS dau_plus_puzzlers
+	COUNT(DISTINCT player) AS dau
 FROM games_plus_puzzle_attempts
 LEFT JOIN public.users ON games_plus_puzzle_attempts.player = users.id
 WHERE NOT (users.internal_bot OR users.id IN (42,43,44,45,46))
 GROUP BY 1)
 
 SELECT
-  dau_report.day,
-  dau_report.dau,
-  dauh_report.dau_h,
-  TRUNC(100.0*dauh_report.dau_h/dau_report.dau,1) AS ratio,
+  dau_omgwords_report.day,
+  dau_omgwords_report.dau_omgwords,
+  dau_omgwords_vs_human_report.dau_omgwords_vs_human,
+  TRUNC(100.0*dau_omgwords_vs_human_report.dau_omgwords_vs_human/dau_omgwords_report.dau_omgwords,1) AS ratio,
   dau_puzzles_report.dau_puzzles,
-  games_plus_puzzles_report.dau_plus_puzzlers
-FROM dau_report
-LEFT JOIN dauh_report ON dau_report.day = dauh_report.day
-LEFT JOIN dau_puzzles_report ON dau_report.day = dau_puzzles_report.day
-LEFT JOIN games_plus_puzzles_report ON dau_report.day = games_plus_puzzles_report.day
+  omgwords_plus_puzzles_report.dau
+FROM dau_omgwords_report
+LEFT JOIN dau_omgwords_vs_human_report ON dau_omgwords_report.day = dau_omgwords_vs_human_report.day
+LEFT JOIN dau_puzzles_report ON dau_omgwords_report.day = dau_puzzles_report.day
+LEFT JOIN omgwords_plus_puzzles_report ON dau_omgwords_report.day = omgwords_plus_puzzles_report.day
 ORDER BY 1 DESC
