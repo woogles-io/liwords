59f1a64dab97dffe9294bfc1955063755ad1b11a
Jesse Day <jessed@berkeley.edu>
2022-05-15 18:47:39 -0700

    finished a per-country MAU query

--- reporting/reporting/mau_reporting_by_country.sql
+++ reporting/reporting/mau_reporting_by_country.sql
@@ -49,29 +49,29 @@ pvp_games as
  
 duplicated_games_between_humans AS
 ((SELECT
-    games.created_at,
-    games.player0_id AS player,
+    pvp_games.created_at,
+    pvp_games.player0_id AS player,
     profiles.country_code
 FROM pvp_games
-LEFT JOIN public.profiles ON games.player0_id = profiles.user_id
+LEFT JOIN public.profiles ON pvp_games.player0_id = profiles.user_id
 )
 UNION ALL
 (SELECT
-    games.created_at,
-    games.player1_id AS player,
+    pvp_games.created_at,
+    pvp_games.player1_id AS player,
     profiles.country_code
 FROM pvp_games
-LEFT JOIN public.profiles ON games.player1_id = profiles.user_id
+LEFT JOIN public.profiles ON pvp_games.player1_id = profiles.user_id
 )),
 
 mau_omgwords_vs_human_report AS
 (SELECT
     DATE_TRUNC('month',duplicated_games_between_humans.created_at) AS month,
     country_code,
-    COUNT(DISTINCT player) AS mau_omgwords
+    COUNT(DISTINCT player) AS mau_omgwords_vs_human
 FROM duplicated_games_between_humans
 GROUP BY 1,2
-ORDER BY 1,3 DESC),
+ORDER BY 1 DESC,3 DESC),
 
 -- Puzzles
 mau_puzzles AS
@@ -97,31 +97,43 @@ ORDER BY 1,3 DESC),
 -- Joint reporting
 games_plus_puzzle_attempts AS
 ((SELECT
-    created_at,
-    player0_id AS player
-FROM public.games)
+    games.created_at,
+    games.player0_id AS player,
+    profiles.country_code
+FROM public.games
+LEFT JOIN public.profiles ON games.player0_id = profiles.user_id
+)
 UNION ALL
 (SELECT
-    created_at,
-    player1_id AS player
-FROM public.games)
+    games.created_at,
+    games.player1_id AS player,
+    profiles.country_code
+FROM public.games
+LEFT JOIN public.profiles ON games.player1_id = profiles.user_id
+)
 UNION ALL
 (SELECT
-   created_at,
-   user_id AS player
-FROM public.puzzle_attempts)),
+    puzzle_attempts.created_at,
+    puzzle_attempts.user_id AS player,
+    profiles.country_code
+FROM public.puzzle_attempts
+LEFT JOIN public.profiles ON puzzle_attempts.user_id = profiles.user_id
+)),
 
 omgwords_plus_puzzles_report AS
 (SELECT
 	DATE_TRUNC('month',games_plus_puzzle_attempts.created_at) AS month,
+    country_code,
 	COUNT(DISTINCT player) AS mau
 FROM games_plus_puzzle_attempts
 LEFT JOIN public.users ON games_plus_puzzle_attempts.player = users.id
 WHERE NOT (users.internal_bot OR users.id IN (42,43,44,45,46))
-GROUP BY 1)
+GROUP BY 1,2
+ORDER BY 1 DESC,3 DESC)
 
 SELECT
   mau_omgwords_report.month,
+  mau_omgwords_report.country_code,
   mau_omgwords_report.mau_omgwords,
   mau_omgwords_vs_human_report.mau_omgwords_vs_human,
   TRUNC(100.0*mau_omgwords_vs_human_report.mau_omgwords_vs_human/mau_omgwords_report.mau_omgwords,1) AS ratio,
@@ -129,6 +141,9 @@ SELECT
   omgwords_plus_puzzles_report.mau
 FROM mau_omgwords_report
 LEFT JOIN mau_omgwords_vs_human_report ON mau_omgwords_report.month = mau_omgwords_vs_human_report.month
+  AND mau_omgwords_report.country_code = mau_omgwords_vs_human_report.country_code
 LEFT JOIN mau_puzzles_report ON mau_omgwords_report.month = mau_puzzles_report.month
+  AND mau_omgwords_report.country_code = mau_puzzles_report.country_code
 LEFT JOIN omgwords_plus_puzzles_report ON mau_omgwords_report.month = omgwords_plus_puzzles_report.month
-ORDER BY 1 DESC
+  AND mau_omgwords_report.country_code = omgwords_plus_puzzles_report.country_code
+ORDER BY 1 DESC,7 DESC
