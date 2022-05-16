763902d7f66b706c4a6419f9e6740bd23176fd50
BriAnna McKissen <briannamckissen@gmail.com>
2022-05-15 19:27:37 -0700

    Shiny new profile

---
+++ liwords-ui/src/profile/gameCard.tsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import moment from 'moment';
+import { Card, Tag, Tooltip } from 'antd';
+import { PlayerAvatar } from '../shared/player_avatar';
+import { Link } from 'react-router-dom';
+import { VariantIcon } from '../shared/variant_icons';
+import { FundOutlined } from '@ant-design/icons';
+import { timeToString } from '../store/constants';
+import { GameMetadata } from '../gameroom/game_info';
+
+type GameCardProps = {
+  game: GameMetadata;
+  userID: string;
+};
+export const GameCard = React.memo((props: GameCardProps) => {
+  const { game, userID } = props;
+  const special = ['Unwoogler', 'AnotherUnwoogler', userID];
+  const {
+    created_at,
+    game_id,
+    players,
+    winner,
+    scores,
+    game_request,
+    game_end_reason,
+    time_control_name,
+  } = game;
+  const whenMoment = moment(created_at || '');
+  const when = (
+    <Tooltip title={whenMoment.format('LLL')}>{whenMoment.fromNow()}</Tooltip>
+  );
+  if (!(players?.length > 1)) {
+    return null;
+  }
+  const userplace =
+    special.indexOf(players[0].user_id) > special.indexOf(players[1].user_id)
+      ? 0
+      : 1;
+  const opponent = players[1 - userplace];
+  const opponentLink = (
+    <div className="opponent-link">
+      <PlayerAvatar
+        player={{
+          user_id: opponent.user_id,
+          nickname: opponent.nickname,
+        }}
+      />
+      <Link to={`/profile/${encodeURIComponent(opponent.nickname)}`}>
+        {players[1 - userplace].nickname}
+      </Link>
+    </div>
+  );
+
+  const challenge = {
+    FIVE_POINT: '+5',
+    TEN_POINT: '+10',
+    SINGLE: 'x1',
+    DOUBLE: 'x2',
+    TRIPLE: 'x3',
+    VOID: 'Void',
+  }[game_request.challenge_rule as string];
+
+  let endReason = '';
+  switch (game_end_reason) {
+    case 'TIME':
+      endReason = 'Time out';
+      break;
+    case 'CONSECUTIVE_ZEROES':
+      endReason = 'Six-zero rule';
+      break;
+    case 'RESIGNED':
+      endReason = 'Resignation';
+      break;
+    case 'FORCE_FORFEIT':
+      endReason = 'Forfeit';
+      break;
+    case 'ABORTED':
+      endReason = 'Aborted';
+      break;
+    case 'CANCELLED':
+      endReason = 'Cancelled';
+      break;
+    case 'TRIPLE_CHALLENGE':
+      endReason = 'Triple challenge';
+      break;
+    case 'STANDARD':
+      endReason = 'Completed';
+  }
+
+  const getDetails = (
+    <div className="detail-icons">
+      {game_request.rules.variant_name === 'wordsmog' ? (
+        <>
+          <VariantIcon vcode="wordsmog" />{' '}
+        </>
+      ) : null}
+      {game_request.rating_mode === 'RATED' ? (
+        <Tooltip title="Rated">
+          <FundOutlined />
+        </Tooltip>
+      ) : null}
+      <Tooltip title="Challenge Mode">
+        <span className={`challenge-rule mode_${challenge}`}>{challenge}</span>
+      </Tooltip>
+      {players[userplace].first && <Tag className="ant-tag-first">1st</Tag>}
+    </div>
+  );
+
+  let result = 'Loss';
+  if (winner === -1) {
+    result = 'Tie';
+  } else if (winner === userplace) {
+    result = 'Win';
+  }
+  const actions = [
+    <Link
+      key="examine-action"
+      to={`/game/${encodeURIComponent(String(game_id ?? ''))}`}
+    >
+      Examine
+    </Link>,
+  ];
+  const scoreDisplay = (
+    <>
+      <div>
+        <h3>
+          {(scores?.[userplace] || 0).toString() +
+            ' - ' +
+            (scores?.[1 - userplace] || 0).toString()}
+        </h3>
+        <p>{when}</p>
+      </div>
+      <Tag className={`ant-tag-${result.toLowerCase()}`}>{result}</Tag>
+    </>
+  );
+  const time = `${time_control_name} ${timeToString(
+    game_request.initial_time_seconds,
+    game_request.increment_seconds,
+    game_request.max_overtime_minutes
+  )}`;
+  return (
+    <Card
+      className={`game-info ${result.toLowerCase()}`}
+      title={scoreDisplay}
+      actions={actions}
+    >
+      {opponentLink}
+      <div className="variant-info">
+        {game_request.lexicon} - <span className="time-control">{time}</span>
+      </div>
+      <p>{endReason}</p>
+      {getDetails}
+    </Card>
+  );
+});
--- liwords-ui/src/profile/profile.tsx
+++ liwords-ui/src/profile/profile.tsx
@@ -15,7 +15,10 @@ import './profile.scss';
 import { BioCard } from './bio';
 import { lexiconCodeToProfileRatingName } from '../shared/lexica';
 import { VariantIcon } from '../shared/variant_icons';
-
+import moment from 'moment';
+import { GameCard } from './gameCard';
+import { GamesHistoryCard } from './games_history';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const screenSizes = require('../base.scss').default;
 
 type Rating = {
@@ -28,9 +31,9 @@ type Rating = {
 type ProfileRatings = { [variant: string]: Rating };
 
 type StatItem = {
-  n: string; // name
+  n?: string; // name
   t: number; // total
-  a: Array<number>; // averages
+  a?: Array<number>; // averages
 };
 
 type Stats = {
@@ -60,9 +63,12 @@ const VariantCard = React.memo((props: VariantCardProps) => {
   const averageTurn = (stats.d1['Score'].t / stats.d1['Turns'].t).toFixed(0);
   const bestPlay = stats.d1['High Turn'].t.toFixed(0);
   const bingos = stats.d1['Bingos'].t.toFixed(0);
+  const lastPlayed =
+    ratings?.ts && moment(new Date(ratings?.ts * 1000)).format('MMM DD, YYYY');
   return (
     <Card className="variant-stats" title={variantToName(variant)}>
       {rating && <h3 className="rating">{rating}</h3>}
+      {rating && <p className="rating-date">on {lastPlayed}</p>}
       {gamesPlayed && (
         <h4 className="stat-item">
           {gamesPlayed}
@@ -100,6 +106,67 @@ const VariantCard = React.memo((props: VariantCardProps) => {
         </h4>
       )}
     </Card>
+  );
+});
+
+type AggregateStatsProps = {
+  stats: ProfileStats | null;
+};
+
+const AggregateStatsCard = React.memo((props: AggregateStatsProps) => {
+  const mergeStats = (statsMaps: { [key: string]: StatItem }[]) => {
+    const ret: { [key: string]: StatItem } = {};
+    statsMaps.forEach((s) => {
+      for (const k in s) {
+        if (ret[k]) {
+          ret[k] = { t: s[k].t + ret[k].t };
+        } else {
+          ret[k] = { t: s[k].t };
+        }
+      }
+    });
+    return ret;
+  };
+  const { stats } = props;
+  if (!stats) {
+    return null;
+  }
+  const player = Object.values(stats).map((a) => a.d1);
+  const totals = mergeStats(player);
+  const gamesPlayed = totals['Games']?.t || 0;
+  const points = totals['Score']?.t || 0;
+  const wins = totals['Wins']?.t || 0;
+  const bingos = totals['Bingos']?.t || 0;
+  const challenges = totals['Challenges Won']?.t || 0;
+  const phonies =
+    totals['Challenged Phonies']?.t + totals['Unchallenged Phonies']?.t;
+  return (
+    <div className="aggregate-stats">
+      <div className="aggregate-item games">
+        <h4>{gamesPlayed}</h4>
+        <p>games played</p>
+      </div>
+      <div className="aggregate-item win">
+        <h4>{wins}</h4>
+        <p>games won</p>
+      </div>
+      <div className="aggregate-item score">
+        <h4>{points}</h4>
+        <p>points scored</p>
+      </div>
+      <div className="aggregate-item bingos">
+        <h4>{bingos}</h4>
+        <p>bingos played</p>
+      </div>
+      <div className="aggregate-item phonies">
+        <h4>{phonies}</h4>
+        <p>phonies played</p>
+      </div>
+      <div className="aggregate-item challenges">
+        <h4>{challenges}</h4>
+        <p>challenges won</p>
+      </div>
+    </div>
   );
 });
 
@@ -151,17 +218,19 @@ const variantToName = (variant: string) => {
 export const PlayerProfile = React.memo(() => {
   const { useState } = useMountedState();
 
-  const gamesPageSize = 20;
+  const gamesPageSize = 24;
   const { username } = useParams();
   const location = useLocation();
   // Show username's profile
   const [ratings, setRatings] = useState<ProfileRatings | null>(null);
   const [stats, setStats] = useState<ProfileStats | null>(null);
   const [userID, setUserID] = useState('');
+  const [userFetched, setUserFetched] = useState(false);
   const [fullName, setFullName] = useState('');
   const [avatarUrl, setAvatarUrl] = useState('');
   const [avatarsEditable, setAvatarsEditable] = useState(false);
   const [bio, setBio] = useState('');
+  const [showGameTable, setShowGameTable] = useState(false);
   const [countryCode, setCountryCode] = useState('');
   const [bioLoaded, setBioLoaded] = useState(false);
   const [recentGames, setRecentGames] = useState<{
@@ -183,6 +252,7 @@ export const PlayerProfile = React.memo(() => {
         }
       )
       .then((resp) => {
+        setUserFetched(true);
         setMissingBirthdate(!resp.data.birth_date);
         setRatings(JSON.parse(resp.data.ratings_json).Data);
         setStats(JSON.parse(resp.data.stats_json).Data);
@@ -194,8 +264,12 @@ export const PlayerProfile = React.memo(() => {
         setBio(resp.data.about);
         setBioLoaded(true);
       })
-      .catch(errorCatcher);
+      .catch((e: AxiosError) => {
+        setUserFetched(true);
+        errorCatcher(e);
+      });
   }, [username, location.pathname]);
+
   const [queriedRecentGamesOffset, setQueriedRecentGamesOffset] =
     useState(recentGamesOffset);
   const reentrancyCheck = useRef<Record<string, never>>();
@@ -264,6 +338,7 @@ export const PlayerProfile = React.memo(() => {
       }
     })();
   }, [username, queriedRecentGamesOffset]);
+
   useEffect(() => {
     // offset and numGames are int32 in the protobuf.
     const maxPage = Math.floor(((1 << 30) * 2 - 1) / gamesPageSize);
@@ -282,6 +357,7 @@ export const PlayerProfile = React.memo(() => {
       };
     }
   }, [recentGamesOffset]);
+
   const fetchPrev = useCallback(() => {
     setRecentGamesOffset((r) => Math.max(r - gamesPageSize, 0));
   }, []);
@@ -301,15 +377,13 @@ export const PlayerProfile = React.memo(() => {
     },
     []
   );
+
   const player = {
     avatar_url: avatarUrl,
     full_name: fullName,
     user_id: userID, // for name-based avatar initial to work
   };
   const avatarEditable = avatarsEditable && viewer === username;
-  useEffect(() => {
-    console.warn(3, recentGames, ratings, stats);
-  }, [recentGames, ratings, stats]);
 
   const variantCards = useMemo(() => {
     const data = [];
@@ -322,7 +396,7 @@ export const PlayerProfile = React.memo(() => {
         });
       }
     }
-    return data
+    const ret = data
       .sort((a, b) => {
         console.log(b, a);
         return (b.stats?.d1['Games']?.t || 0) - (a.stats?.d1['Games']?.t || 0);
@@ -330,80 +404,221 @@ export const PlayerProfile = React.memo(() => {
       .map((d) => {
         return <VariantCard key={d.variant} {...d} />;
       });
+    // Mobile swipe requires an even number of cards
+    if (ret.length % 2) {
+      ret.push(<div />);
+    }
+    return ret;
   }, [ratings, stats]);
+
+  const puzzleCards = useMemo(() => {
+    const data = [];
+    for (const variant in ratings) {
+      if (ratings.hasOwnProperty(variant) && variant.includes('puzzle')) {
+        data.push({
+          variant: variant,
+          ratings: ratings[variant],
+        });
+      }
+    }
+    if (!ratings) {
+      return;
+    }
+    const ret = data.map((v) => {
+      const rating = v.ratings?.r.toFixed(0);
+      const lastPlayed =
+        v.ratings?.ts &&
+        moment(new Date(v.ratings?.ts * 1000)).format('MMM DD, YYYY');
+      return (
+        <Card
+          className="puzzle-stats"
+          title={variantToName(v.variant)}
+          key={v.variant}
+        >
+          {rating && <h3 className="rating">{rating}</h3>}
+          {rating && <p className="rating-date">on {lastPlayed}</p>}
+        </Card>
+      );
+    });
+    // Mobile swipe requires an even number of cards
+    if (ret.length % 2) {
+      ret.push(<div />);
+    }
+    return ret;
+  }, [ratings]);
+
+  const gameCards = useMemo(() => {
+    if (!recentGames?.array) {
+      return [];
+    }
+    const ret = recentGames?.array
+      ?.filter((g) => g.players?.length && g.game_end_reason !== 'CANCELLED')
+      .map((g) => <GameCard game={g} key={g.game_id} userID={userID} />);
+    // Mobile swipe requires an even number of cards
+    if (ret.length % 2) {
+      ret.push(<div />);
+    }
+    return ret;
+  }, [recentGames, userID]);
 
   return (
     <>
       <TopBar />
-      <div className="profile">
-        <header>
-          <div>
-            <div className="user-info">
-              <PettableAvatar>
-                <PlayerAvatar
-                  player={player}
-                  editable={avatarEditable}
-                  username={username}
-                />
-                <h3>
-                  {username && viewer !== username ? (
-                    <UsernameWithContext
-                      omitProfileLink
-                      omitSendMessage
-                      fullName={fullName}
-                      includeFlag
-                      username={username}
-                      userID={userID}
-                      showModTools
-                      moderate={moderateUser}
-                    />
-                  ) : (
-                    <span className="user">
-                      <span>{fullName || username}</span>
-                      <DisplayFlag countryCode={countryCode} />
-                    </span>
-                  )}
-                </h3>
-              </PettableAvatar>
-            </div>
-            {!(missingBirthdate && viewer === username) && (
-              <BioCard bio={bio} bioLoaded={bioLoaded} />
-            )}
-            {missingBirthdate && viewer === username && (
-              <div className="bio">
-                <Link to={'/settings/personal'}>
-                  Let us know your birthdate
-                </Link>{' '}
-                to share your bio and details
+      {userID && (
+        <div className="profile">
+          <header>
+            <div>
+              <div className="user-info">
+                <PettableAvatar>
+                  <PlayerAvatar
+                    player={player}
+                    editable={avatarEditable}
+                    username={username}
+                  />
+                  <h3>
+                    {username && viewer !== username ? (
+                      <UsernameWithContext
+                        omitProfileLink
+                        omitSendMessage
+                        fullName={fullName}
+                        includeFlag
+                        username={username}
+                        userID={userID}
+                        showModTools
+                        moderate={moderateUser}
+                      />
+                    ) : (
+                      <span className="user">
+                        <span>{fullName || username}</span>
+                        <DisplayFlag countryCode={countryCode} />
+                      </span>
+                    )}
+                  </h3>
+                </PettableAvatar>
               </div>
-            )}
-          </div>
-        </header>
-        <h2>Game Ratings</h2>
-        <Carousel
-          arrows
-          className="variant-items"
-          dots={{
-            className: 'dot',
-          }}
-          slidesToScroll={4}
-          slidesToShow={4}
-          swipeToSlide
-          swipe
-          responsive={[
-            {
-              breakpoint: screenSizes.screenSizeTablet - 1,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-              },
-            },
-          ]}
-        >
-          {variantCards}
-        </Carousel>
-        <h2>Puzzle Ratings</h2>
-      </div>
+              {!(missingBirthdate && viewer === username) && (
+                <BioCard bio={bio} bioLoaded={bioLoaded} />
+              )}
+              {missingBirthdate && viewer === username && (
+                <div className="bio">
+                  <Link to={'/settings/personal'}>
+                    Let us know your birthdate
+                  </Link>{' '}
+                  to share your bio and details
+                </div>
+              )}
+            </div>
+            <AggregateStatsCard stats={stats} />
+          </header>
+          {variantCards?.length > 0 && (
+            <>
+              <h2>Game Ratings</h2>
+              <Carousel
+                arrows
+                className="variant-items"
+                slidesToScroll={4}
+                slidesToShow={4}
+                swipeToSlide
+                swipe
+                infinite={false}
+                responsive={[
+                  {
+                    breakpoint: screenSizes.screenSizeTablet - 1,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2,
+                    },
+                  },
+                ]}
+              >
+                {variantCards}
+              </Carousel>
+            </>
+          )}
+          {!!puzzleCards?.length && (
+            <>
+              <h2>Puzzle Ratings</h2>
+              <Carousel
+                arrows
+                className="puzzle-items"
+                slidesToScroll={4}
+                slidesToShow={4}
+                swipeToSlide
+                swipe
+                infinite={false}
+                responsive={[
+                  {
+                    breakpoint: screenSizes.screenSizeTablet - 1,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2,
+                    },
+                  },
+                ]}
+              >
+                {puzzleCards}
+              </Carousel>
+            </>
+          )}
+          {!!gameCards?.length && (
+            <>
+              <h2>Recent games</h2>
+              <Carousel
+                arrows
+                className="game-items"
+                slidesToScroll={4}
+                slidesToShow={4}
+                rows={gameCards.length > 4 ? 2 : 1}
+                swipeToSlide
+                swipe
+                infinite={false}
+                responsive={[
+                  {
+                    breakpoint: screenSizes.screenSizeTablet - 1,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2,
+                      rows: gameCards.length > 2 ? 2 : 1,
+                    },
+                  },
+                ]}
+              >
+                {gameCards}
+              </Carousel>
+            </>
+          )}
+          <p
+            className="show-games-toggle"
+            onClick={() => {
+              setShowGameTable((x) => !x);
+            }}
+          >
+            {!showGameTable ? 'Show game table' : 'Hide game table'}
+          </p>
+          {username && showGameTable && (
+            <GamesHistoryCard
+              games={recentGames.array}
+              username={username}
+              userID={userID}
+              fetchPrev={recentGamesOffset > 0 ? fetchPrev : undefined}
+              fetchNext={
+                recentGames.array.length < recentGames.numGames
+                  ? undefined
+                  : fetchNext
+              }
+              currentOffset={recentGames.offset}
+              currentPageSize={recentGames.numGames}
+              desiredOffset={recentGamesOffset}
+              desiredPageSize={gamesPageSize}
+              onChangePageNumber={handleChangePageNumber}
+            />
+          )}
+        </div>
+      )}
+
+      {userFetched && !userID && (
+        <div className="not-found">User not found.</div>
+      )}
     </>
   );
 });
