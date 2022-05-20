876d59d70e1c6c9f1fe6df6bfe024cc84ca32f62
BriAnna McKissen <briannamckissen@gmail.com>
2022-05-19 17:43:40 -0700

    GetNextClosestRatingPuzzleId does not work. Checking this in to get help from the backend guys.

--- liwords-ui/src/puzzles/puzzle.tsx
+++ liwords-ui/src/puzzles/puzzle.tsx
@@ -33,6 +33,8 @@ import {
   SubmissionResponse,
   StartPuzzleIdRequest,
   StartPuzzleIdResponse,
+  NextClosestRatingPuzzleIdRequest,
+  NextClosestRatingPuzzleIdResponse,
 } from '../gen/api/proto/puzzle_service/puzzle_service_pb';
 import { sortTiles } from '../store/constants';
 import { Notepad, NotepadContextProvider } from '../gameroom/notepad';
@@ -171,16 +173,10 @@ export const SinglePuzzle = (props: Props) => {
         setShowLexiconModal(true);
         return;
       }
-      let req, respType, method;
-      if (firstLoad === true) {
-        req = new StartPuzzleIdRequest();
-        respType = StartPuzzleIdResponse;
-        method = 'GetStartPuzzleId';
-      } else {
-        req = new NextPuzzleIdRequest();
-        respType = NextPuzzleIdResponse;
-        method = 'GetNextPuzzleId';
-      }
+      const req = new NextClosestRatingPuzzleIdRequest();
+      const respType = NextClosestRatingPuzzleIdResponse;
+      const method = 'GetNextClosestRatingPuzzleId';
+
       req.setLexicon(userLexicon);
       try {
         const resp = await postProto(
