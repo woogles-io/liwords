fb8b22fcaf27e2fc5cef20a86d4b8217ba9e2018
Jesse Day <jessed@berkeley.edu>
2022-06-23 17:24:41 -0700

    differentiate puzzles seen vs attempted in puzzle reporting

--- reporting/puzzles/most_puzzles_solved.sql
+++ reporting/puzzles/most_puzzles_solved.sql
@@ -1,13 +1,16 @@
-select
+SELECT
   username,
-  (select COUNT(*) from puzzle_attempts where user_id = users.id) as puzzles_attempted,
-  (select COUNT(*) from puzzle_attempts where user_id = users.id AND correct = true) as correct_puzzles,
-  100.0*(select COUNT(*) from puzzle_attempts where user_id = users.id AND correct = true)/(select COUNT(*) from puzzle_attempts where user_id = users.id) AS puzzle_solved_pct,
-  ratings->'Data'->'CSW19.puzzle.corres'->'r' as csw_rating,
-  ratings->'Data'->'NWL18.puzzle.corres'->'r' as nwl_rating
+  (SELECT COUNT(*) FROM puzzle_attempts WHERE user_id = users.id) as puzzles_seen,
+  (SELECT COUNT(*) FROM puzzle_attempts WHERE user_id = users.id AND correct IS NOT NULL) as puzzles_attempted,
+  (SELECT COUNT(*) FROM puzzle_attempts WHERE user_id = users.id AND correct = true) as puzzles_solved,
+  100.0*(SELECT COUNT(*) FROM puzzle_attempts WHERE user_id = users.id AND correct = true)/(SELECT COUNT(*) FROM puzzle_attempts WHERE user_id = users.id) AS puzzle_solved_pct,
+  CASE WHEN ratings->'Data'->'CSW19.puzzle.corres'->'r' IS NULL THEN '0'
+    ELSE ratings->'Data'->'CSW19.puzzle.corres'->'r' END AS csw_rating,
+  CASE WHEN ratings->'Data'->'NWL18.puzzle.corres'->'r' IS NULL THEN '0'
+    ELSE ratings->'Data'->'NWL18.puzzle.corres'->'r' END AS nwl_rating
 from users, profiles 
 where
   users.id = profiles.user_id 
 AND
   users.id in (select distinct(user_id) from puzzle_attempts)
-order by csw_rating desc;
+order by nwl_rating desc;
