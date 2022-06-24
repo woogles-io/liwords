6a7c8dbc1abcd2c2f3d217cb8e84121415d8721d
Jesse Day <jessed@berkeley.edu>
2022-06-23 23:13:01 -0700

    added extra per-puzzle stats, also realized that logging into the homepage now counts as seeing a puzzle

--- reporting/puzzles/puzzle_ratings.sql
+++ reporting/puzzles/puzzle_ratings.sql
@@ -1,9 +1,25 @@
-SELECT
+WITH base_query AS
+(SELECT
   id,
   uuid,
   lexicon,
   rating->'r' AS rating,
   rating->'rd' AS rating_deviation,
-  (SELECT COUNT(*) from puzzle_attempts where puzzle_attempts.puzzle_id = puzzles.id) AS number_of_attempts
-FROM puzzles
-ORDER BY number_of_attempts DESC
+  (SELECT COUNT(*) FROM puzzle_attempts WHERE puzzle_attempts.puzzle_id = puzzles.id) AS times_seen,
+  (SELECT COUNT(*) FROM puzzle_attempts WHERE puzzle_attempts.puzzle_id = puzzles.id 
+     AND puzzle_attempts.correct IS NOT NULL) AS times_attempted,
+  (SELECT COUNT(*) FROM puzzle_attempts WHERE puzzle_attempts.puzzle_id = puzzles.id 
+     AND puzzle_attempts.correct = 'true') AS times_correct
+FROM puzzles)
+
+SELECT
+  *,
+  times_seen-times_attempted AS times_skipped,
+  CASE WHEN times_attempted != 0 
+    THEN ROUND(100.0*(times_seen-times_attempted)/times_seen,1)
+	ELSE 0 END AS skip_pct,
+  CASE WHEN times_attempted != 0
+    THEN ROUND(100.0*times_correct/times_attempted,1)
+	ELSE 0 END AS correct_attempt_pct
+FROM base_query
+ORDER BY skip_pct DESC
