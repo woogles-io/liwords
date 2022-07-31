72e88f10f900387cecd363accaff04e40a9d12a7
Andy Kurnia <andykurn@gmail.com>
2022-07-31 10:31:41 +0800

    enable variants if wordsmog was enabled

--- liwords-ui/src/index.tsx
+++ liwords-ui/src/index.tsx
@@ -19,6 +19,16 @@ window.console.info(
   'Woogles.io is open source! https://github.com/domino14/liwords'
 );
 
+if (localStorage.getItem('enableWordSmog')) {
+  if (!localStorage.getItem('enableVariants')) {
+    localStorage.setItem(
+      'enableVariants',
+      localStorage.getItem('enableWordSmog')
+    );
+  }
+  localStorage.removeItem('enableWordSmog');
+}
+
 // Scope the variables declared here.
 {
   // Adjust this constant accordingly.
