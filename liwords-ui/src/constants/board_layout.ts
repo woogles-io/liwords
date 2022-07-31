0e63aa494081eb326e77acea8578dc4218c36a19
Andy Kurnia <andykurn@gmail.com>
2022-07-31 14:01:56 +0800

    fix zomgwords board layout

--- liwords-ui/src/constants/board_layout.ts
+++ liwords-ui/src/constants/board_layout.ts
@@ -57,7 +57,7 @@ export const SuperCrosswordGameGridLayout = [
   `=      -     -      =`,
   ` -  "   "   "   "  - `,
   `  -  '   ' '   '  -  `,
-  `'  =  '   -   '  =   `,
+  `'  =  '   -   '  =  '`,
   `  -  '   ' '   '  -  `,
   ` -  "   "   "   "  - `,
   `=      -     -      =`,
