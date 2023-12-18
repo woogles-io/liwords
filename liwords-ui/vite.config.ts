460d1578f7090654ba0026ba810447e7489ff4f8
César Del Solar <delsolar@gmail.com>
2023-12-17 22:42:44 -0500

    try wasm load

--- liwords-ui/vite.config.ts
+++ liwords-ui/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import ViteSassPlugin from 'vite-plugin-sass';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: '',
-  plugins: [react(), ViteSassPlugin()],
+  plugins: [react(), ViteSassPlugin(), wasm(), topLevelAwait()],
   server: {
-    // this ensures that the browser opens upon server start
-    open: true,
     // this sets a default port to 3000
     port: 3000,
   },
