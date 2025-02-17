98d17c42cf39cc1aabeecf1278e2decd11a311c2
Ben Muschol <benmuschol@gmail.com>
2025-02-16 21:57:42 -0500

    update

--- db/migrations/202502161828_tournament_scheduled_start_and_end.down.sql
+++ db/migrations/202502161828_tournament_scheduled_start_and_end.down.sql
@@ -1,6 +1,7 @@
-DROP INDEX idx_tournaments_scheduled_start_time;
-DROP INDEX idx_tournaments_scheduled_end_time;
+BEGIN;
 
 ALTER TABLE tournaments
 DROP COLUMN scheduled_start_time,
 DROP COLUMN scheduled_end_time;
+
+COMMIT;
