3bca28c5bca32545163c536e27fe11d3f4b4a24c
Andy Kurnia <andykurn@gmail.com>
2025-02-22 11:38:03 +0800

    fix needless fmt.Sprintf

--- pkg/cwgame/api_test.go
+++ pkg/cwgame/api_test.go
@@ -1115,7 +1115,7 @@ func TestResign(t *testing.T) {
 
 	for _, tc := range testcases {
 		tc := tc
-		t.Run(fmt.Sprintf(tc.name), func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			is := is.New(t)
 			gdoc := loadGDoc(documentfile)
 
