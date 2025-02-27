80558d2f589ee431c30969c49034fb1f43dc5534
César Del Solar <delsolar@gmail.com>
2025-02-26 23:02:53 -0500

    start adding new game table migrations

---
+++ db/migrations/202502260432_game_table_changes.up.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+ALTER TABLE games ADD COLUMN game_request jsonb NOT NULL DEFAULT '{}';
+ALTER TABLE games ADD COLUMN history_in_s3 boolean NOT NULL DEFAULT false;
+
+CREATE TABLE game_players (
+    game_id integer NOT NULL,
+    player_id integer NOT NULL,
+    player_index SMALLINT, -- 0 or 1 for first or second
+    FOREIGN KEY (game_id) REFERENCES games (id),
+    FOREIGN KEY (player_id) REFERENCES users (id),
+    PRIMARY KEY (game_id, player_id);
+);
+
+CREATE TABLE active_game_events (
+    game_id integer NOT NULL,
+    event_idx integer NOT NULL,
+    event JSONB NOT NULL DEFAULT '{}',
+
+    FOREIGN KEY (game_id) REFERENCES games (id)
+);
+
+
+COMMIT;
