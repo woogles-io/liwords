659c8b3f1e27c956f143e481b8b74593db3bc293
César Del Solar <delsolar@gmail.com>
2025-04-16 09:14:34 -0400

    fix lint

--- liwords-ui/rsbuild.config.ts
+++ liwords-ui/rsbuild.config.ts
@@ -4,7 +4,12 @@ import { pluginSass } from "@rsbuild/plugin-sass";
 import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default {
-  plugins: [pluginEslint(), pluginReact(), pluginSass(), pluginSvgr()],
+  plugins: [
+    pluginEslint({ eslintPluginOptions: { configType: "flat" } }),
+    pluginReact(),
+    pluginSass(),
+    pluginSvgr(),
+  ],
   dev: { client: { overlay: false } },
   html: {
     template: "./index.html",
