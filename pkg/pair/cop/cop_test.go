a2d1b95c675b5e079769fb996985db1fbf6a16af
jvc56 <jvc56@cornell.edu>
2025-07-19 23:01:58 -0400

    Remove print statement from COP tests

--- pkg/pair/cop/cop_test.go
+++ pkg/pair/cop/cop_test.go
@@ -698,7 +698,6 @@ func TestCOPConstraintPolicies(t *testing.T) {
 	resp.Pairings[11] = 11
 	pairtestutils.AddRoundPairings(req, resp.Pairings)
 	resp = cop.COPPair(ctx, req)
-	fmt.Println(resp.Log)
 	is.Equal(resp.Pairings[0], int32(4))
 	is.Equal(resp.Pairings[4], int32(0))
 	is.Equal(resp.Pairings[1], int32(1))
