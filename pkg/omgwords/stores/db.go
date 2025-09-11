849a9ce790d661ac5c1e8905194a5b3fc4413d71
César Del Solar <delsolar@gmail.com>
2025-09-11 00:25:16 -0400

    fix gamerequest handling for annotated games

--- pkg/omgwords/stores/db.go
+++ pkg/omgwords/stores/db.go
@@ -11,6 +11,7 @@ import (
 	"github.com/woogles-io/liwords/pkg/stores/common"
 	"github.com/woogles-io/liwords/pkg/stores/models"
 	"github.com/woogles-io/liwords/rpc/api/proto/ipc"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -60,11 +61,15 @@ func (s *DBStore) CreateAnnotatedGame(ctx context.Context, creatorUUID string, g
 	if err != nil {
 		return err
 	}
+	jsonReq, err := protojson.Marshal(req)
+	if err != nil {
+		return err
+	}
 	// Also insert it into the old games table. We will need to migrate this.
 	_, err = tx.Exec(ctx, `
-	INSERT INTO games (created_at, updated_at, uuid, type, quickdata, request)
-	VALUES (NOW(), NOW(), $1, $2, $3, $4)
-	`, gameUUID, ipc.GameType_ANNOTATED, quickdata, request)
+	INSERT INTO games (created_at, updated_at, uuid, type, quickdata, request, game_request)
+	VALUES (NOW(), NOW(), $1, $2, $3, $4, $5)
+	`, gameUUID, ipc.GameType_ANNOTATED, quickdata, request, jsonReq)
 	if err != nil {
 		return err
 	}
@@ -205,7 +210,7 @@ func (s *DBStore) GamesForEditor(ctx context.Context, editorID string, unfinishe
 	if editorID == "" {
 		query = `
 		SELECT game_uuid, creator_uuid, username,
-			private_broadcast, quickdata, request, games.created_at
+			private_broadcast, quickdata, request, game_request, games.created_at
 		FROM annotated_game_metadata
 		JOIN games ON games.uuid = annotated_game_metadata.game_uuid
 		JOIN users ON users.uuid = annotated_game_metadata.creator_uuid
@@ -222,7 +227,9 @@ func (s *DBStore) GamesForEditor(ctx context.Context, editorID string, unfinishe
 		}
 	} else {
 		query = `
-		SELECT game_uuid, creator_uuid, 'dummyusername', private_broadcast, quickdata, request, created_at FROM annotated_game_metadata
+		SELECT game_uuid, creator_uuid, 'dummyusername', private_broadcast,
+			quickdata, request, game_request, created_at
+		FROM annotated_game_metadata
 		JOIN games ON games.uuid = annotated_game_metadata.game_uuid
 		WHERE creator_uuid = $1 AND done = $2
 		ORDER BY created_at DESC
@@ -256,7 +263,7 @@ func (s *DBStore) GamesForEditor(ctx context.Context, editorID string, unfinishe
 		if quickdata == nil {
 			continue // although this shouldn't happen
 		}
-		
+
 		// Unmarshal game request
 		var lexicon string
 		gamereq, err := common.UnmarshalGameRequest(gameRequestBytes, requestBytes)
@@ -265,7 +272,7 @@ func (s *DBStore) GamesForEditor(ctx context.Context, editorID string, unfinishe
 		} else if gamereq != nil {
 			lexicon = gamereq.Lexicon
 		}
-		
+
 		games = append(games, &BroadcastGame{
 			GameUUID:        uuid,
 			CreatorUUID:     creatorUUID,
--- pkg/stores/common/gamerequest.go
+++ pkg/stores/common/gamerequest.go
@@ -2,6 +2,8 @@ package common
 
 import (
 	"fmt"
+
+	"github.com/rs/zerolog/log"
 	pb "github.com/woogles-io/liwords/rpc/api/proto/ipc"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
@@ -14,6 +16,7 @@ import (
 func UnmarshalGameRequest(gameRequestBytes []byte, requestBytes []byte) (*pb.GameRequest, error) {
 	// First try to use the jsonb data if it exists
 	if len(gameRequestBytes) > 0 {
+		log.Debug().Bytes("grbts", gameRequestBytes).Msg("Unmarshaling GameRequest from jsonb column")
 		gamereq := &pb.GameRequest{}
 		// Try protojson unmarshal (for JSONB data)
 		err := protojson.Unmarshal(gameRequestBytes, gamereq)
@@ -21,15 +24,8 @@ func UnmarshalGameRequest(gameRequestBytes []byte, requestBytes []byte) (*pb.Gam
 			// Successfully unmarshaled and has valid data
 			return gamereq, nil
 		}
-		// If protojson failed, the data might still be protobuf in the jsonb column
-		// (shouldn't happen but be defensive)
-		gamereq = &pb.GameRequest{}
-		err = proto.Unmarshal(gameRequestBytes, gamereq)
-		if err == nil && gamereq.Rules != nil {
-			return gamereq, nil
-		}
+		log.Debug().Err(err).Msg("Failed to unmarshal full GameRequest from jsonb column, falling back to bytea")
 	}
-
 	// Fall back to bytea protobuf data
 	if len(requestBytes) == 0 {
 		return nil, fmt.Errorf("no request data available in either jsonb or bytea format")
