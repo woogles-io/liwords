55ea5ae8ce861e0d35553e159ebf57366dcbeddb
Andy Kurnia <andykurn@gmail.com>
2025-12-28 19:19:26 +0800

    link season games modal to profile

--- liwords-ui/src/leagues/player_game_history_modal.tsx
+++ liwords-ui/src/leagues/player_game_history_modal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 import { Modal, Table, Tag, Spin } from "antd";
 import { useQuery } from "@connectrpc/connect-query";
 import { getPlayerSeasonGames } from "../gen/api/proto/league_service/league_service-LeagueService_connectquery";
@@ -88,7 +89,14 @@ export const PlayerGameHistoryModal: React.FC<PlayerGameHistoryModalProps> = ({
 
   return (
     <Modal
-      title={`${username}'s Season ${seasonNumber} Games`}
+      title={
+        <React.Fragment>
+          <Link to={`/profile/${encodeURIComponent(username)}`}>
+            {username}
+          </Link>
+          's Season {seasonNumber} Games
+        </React.Fragment>
+      }
       open={visible}
       onCancel={onClose}
       footer={null}
