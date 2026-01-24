9c62b2530f0d9ca3f9f4e28cd747dbfd227a9838
Andy Kurnia <andykurn@gmail.com>
2026-01-25 01:16:49 +0800

    mock do-nothing ResizeObserver to allow testing

--- liwords-ui/src/setupTests.ts
+++ liwords-ui/src/setupTests.ts
@@ -1,1 +1,18 @@
 import "@testing-library/jest-dom/vitest";
+import { beforeAll } from "vitest";
+
+// https://github.com/jsdom/jsdom/issues/3368#issuecomment-1147970817
+
+beforeAll(() => {
+  global.ResizeObserver = class ResizeObserver {
+    observe() {
+      // do nothing
+    }
+    unobserve() {
+      // do nothing
+    }
+    disconnect() {
+      // do nothing
+    }
+  };
+});
