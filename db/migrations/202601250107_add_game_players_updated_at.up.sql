136100a5727f9f3bdfdc08a9589480eae934f583
César Del Solar <delsolar@gmail.com>
2026-03-01 11:48:36 -0500

    fix add game players index comment

--- db/migrations/202601250107_add_game_players_updated_at.up.sql
+++ db/migrations/202601250107_add_game_players_updated_at.up.sql
@@ -2,6 +2,6 @@ alter table game_players add if not exists updated_at timestamptz null;
 
 -- please run these manually outside of transactions:
 
--- create index concurrently if not exists idx_game_players_updated_at on game_players (updated_at);
+-- create index concurrently if not exists idx_game_players_player_updated on game_players (player_id, updated_at DESC);
 
 SELECT '202601250107_add_game_players_updated_at.up.sql documented - manual execution required' AS migration_status;
