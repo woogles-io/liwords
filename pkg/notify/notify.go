6c2cd1634dd2e07ba3668c8fe579dd237ad70d23
César Del Solar <delsolar@gmail.com>
2026-04-15 08:55:04 -0400

    sync start discord msg

--- pkg/notify/notify.go
+++ pkg/notify/notify.go
@@ -13,16 +13,19 @@ func Post(message string, token string) {
 	// Errors should not be fatal, just log them
 	if err != nil {
 		log.Err(err).Str("error", err.Error()).Msg("mod-action-discord-notification-marshal")
+		return
+	}
+
+	// Send synchronously to ensure it completes before caller exits
+	// (important for short-lived processes like maintenance tasks)
+	resp, err := http.Post(token, "application/json", bytes.NewBuffer(requestBody))
+	// Errors should not be fatal, just log them
+	if err != nil {
+		log.Err(err).Str("error", err.Error()).Msg("notification-post-error")
+	} else if resp.StatusCode != 204 { // No Content
+		// We do not expect any other response
+		log.Err(err).Str("status", resp.Status).Msg("notification-post-bad-response")
 	} else {
-		go func() {
-			resp, err := http.Post(token, "application/json", bytes.NewBuffer(requestBody))
-			// Errors should not be fatal, just log them
-			if err != nil {
-				log.Err(err).Str("error", err.Error()).Msg("notification-post-error")
-			} else if resp.StatusCode != 204 { // No Content
-				// We do not expect any other response
-				log.Err(err).Str("status", resp.Status).Msg("notification-post-bad-response")
-			}
-		}()
+		log.Debug().Msg("discord-notification-sent-successfully")
 	}
 }
